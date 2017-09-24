cc323d5fac11a80c1f9323affe6e51f243da613b
Shimizu <tomoaki.shimizu@tks2.co.jp>
2017-09-24 20:05:36 +0900

    画面遷移を用意
    Signed-off-by: Shimizu <tomoaki.shimizu@tks2.co.jp>

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	void Start () {
+    }
+	
+	void Update () {
+	}
+
+    public void TransitionToHome() {
+        SceneManager.LoadScene("Home");
+    }
+
+    public void TransitionToCredit() {
+        SceneManager.LoadScene("Credit");
+    }
+
+    public void TransitionToRoom() {
+        SceneManager.LoadScene("Room");
+    }
+
+    public void TransitionToBar() {
+        SceneManager.LoadScene("Bar");
+    }
+
+    public void TransitionToResult() {
+        SceneManager.LoadScene("Result");
+    }
+
+    public void TransitionToSample() {
+        SceneManager.LoadScene("SampleGame");
+    }
+}
---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour {
+    private GameController gameController = null;
+
+    void Start () {
+        gameController = GameObject.Find("GameController").GetComponent<GameController>();
+
+        Observable.Timer(TimeSpan.FromSeconds(3)).Subscribe(_ => {
+            gameController.TransitionToHome();
+        }).AddTo(this);
+    }
+
+    void Update () {
+	}
+}
