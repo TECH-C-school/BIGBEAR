a6972fb6dcf3022746883561880cee386cf2fbf8
ryoya0914 <uedaryoya0914@gmail.com>
2017-10-19 14:40:04 +0900

    Update GameController.cs

--- Assets/Scripts/Game01/GameController.cs
+++ Assets/Scripts/Game01/GameController.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+//test
 namespace Assets.Scripts.Game01 {
     public class GameController : MonoBehaviour {
         public void TransitionToResult() {
