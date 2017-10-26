d302650d2cd4d7069b8dad6f84602a0017887d7e
W301-06\student <16d3170001ae@edu.tech.ac.jp>
2017-10-26 20:53:15 +0900

    Add Drag Test

--- Assets/Scripts/Game02/GameController.cs
+++ Assets/Scripts/Game02/GameController.cs
@@ -2,32 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Linq;
-using UniRx.Triggers;
-using UniRx;
-using DG.Tweening;
+
 
 namespace Assets.Scripts.Game02
 {
     public class GameController : MonoBehaviour
     {
-        
-        private void Start()
-        {
-
-            GetComponent<ObservableDragTrigger>().OnDragAsObservable().Subscribe(_=>
-
-            {
-
-            });
-        }
-
-        private void Update()
-        {
-            
-        }
-
-
         public void TransitionToResult()
         {
             SceneManager.LoadScene("Result");
---
+++ Assets/Scripts/Game02/PlayerControll.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+using UniRx.Triggers;
+using UniRx;
+using DG.Tweening;
+
+namespace Assets.Scripts.Game02
+{
+    public class PlayerControll : MonoBehaviour
+    {
+
+        [SerializeField]
+        ObservableEventTrigger trigger;
+
+        []
+
+        // Use this for initialization
+        void Start()
+        {
+#if !UNITY_EDITOR
+            trigger.OnDragAsObservable()
+                .Subscribe(pointerEventData =>
+                {
+                    Debug.Log("a");
+                }).AddTo(this);
+#endif
+#if UNITY_EDITOR
+            trigger.OnMouseDragAsObservable()
+                 .Subscribe(pointerEventData =>
+                 {
+                     
+                 }).AddTo(this);
+#endif
+        }
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
