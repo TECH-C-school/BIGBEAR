633476ae355f0a37ff9fed105a0b1f7c35de67e1
student <touhu201@gmail.com>
2017-10-26 15:41:34 +0900

    Update Scene And Update GameContriller

--- Assets/Scripts/Game02/GameController.cs
+++ Assets/Scripts/Game02/GameController.cs
@@ -2,10 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Linq;
+using UniRx.Triggers;
+using UniRx;
+using DG.Tweening;
 
-namespace Assets.Scripts.Game02 {
-    public class GameController : MonoBehaviour {
-        public void TransitionToResult() {
+namespace Assets.Scripts.Game02
+{
+    public class GameController : MonoBehaviour
+    {
+        
+        private void Start()
+        {
+
+            GetComponent<ObservableDragTrigger>().OnDragAsObservable().Subscribe(_=>
+
+            {
+
+            });
+        }
+
+        private void Update()
+        {
+            
+        }
+
+
+        public void TransitionToResult()
+        {
             SceneManager.LoadScene("Result");
         }
     }
