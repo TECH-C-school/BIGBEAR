249d080a960aa9ecc99e550733ba16396fd26d1b
小泉　勇人 <yoshika@yoshika-no-MacBook-Air.local>
2017-10-26 15:05:14 +0900

    LevelSelect.csの作成

---
+++ Assets/Scripts/Bar06/LevelSelect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Bar06{
+	public class LevelSelect : MonoBehaviour {
+
+		[HideInInspector]
+		public bool isEasy,isNormal,isHard = false;
+
+		//----------------------
+		// どれかの難易度が選択されたらそれに応じたフラグを立てる
+		public void TapEasy()
+		{
+			isEasy = true;
+		}
+
+		public void TapNormal()
+		{
+			isNormal = true;
+		}
+
+		public void TapHard()
+		{
+			isHard = true;
+		}
+		//----------------------
+	}
+}
