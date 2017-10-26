71e5792e9a3047ddc975935ef13ed2a7ce7005a5
302-25\student <musisi4474@gmail.com>
2017-10-26 15:40:45 +0900

    アニメーションを作りました

---
+++ Assets/Scripts/Game07/TimeCount.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Game07
+{
+    public class TimeCount : MonoBehaviour
+    {
+
+        float times = 60;
+
+        void Start()
+        {
+            GetComponent<Text>().text = ((int)times).ToString();
+        }
+
+
+        void Update()
+        {
+             times -= Time.deltaTime;
+
+            if (times == 0)
+            {
+
+            }
+        }
+    }
+}
