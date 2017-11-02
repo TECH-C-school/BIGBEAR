09c85cd7f91e3980ab0931e1e355e8772efb03b4
unknown <>
2017-11-02 15:36:21 +0900

    移動処理製作

---
+++ Assets/Scripts/Game08/Enemy/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position += new Vector3(-0.05f, 0f ,0f);
+       
+	}
+}
