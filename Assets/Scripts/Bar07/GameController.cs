a184379f8cee94d98f799645e086ee2923c48274
302-11\student <kamikawaatuya@gamil.com>
2017-11-09 15:43:08 +0900

    addtekst

--- Assets/Scripts/Bar07/GameController.cs
+++ Assets/Scripts/Bar07/GameController.cs
@@ -5,14 +5,28 @@ using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Bar07 {
     public class GameController : MonoBehaviour {
-        enum Days
-        {
+		enum card
+
+		   {
             Clover1, Clover2, Clover3, Clover4, Clover5, Clover6, Clover7, Clover8, Clover9, Clover10, Clover11, Clover12, Clover13,
             Diamond1, Diamond2, Diamond3, Diamond4, Diamond5, Diamond6, Diamond7, Diamond8, Diamond9, Diamond10, Diamond11, Diamond12, Diamond13,
             Heart1, Heart2, Heart3, Heart4, Heart5, Heart6, Heart7, Heart8, Heart9, Heart10, Heart11, Heart12, Heart13,
             Spade1, Spade2, Spade3, Spade4, Spade5, Spade6, Spade7, Spade8, Spade9, Spade10, Spade11, Spade12, Spade13
-        }
-                    
+		};			
+		void distribution()
+		{
+			var card = (card)Random.Range (0, 52);
+			Debug.Log (card.ToString ());
+		}
+
+		void Start()
+		{
+			for (i = 0; i < 4; i++) 
+			{
+				distribution ();
+			}
+		}
+
         public void TransitionToResult() {
             SceneManager.LoadScene("Result");
         }
