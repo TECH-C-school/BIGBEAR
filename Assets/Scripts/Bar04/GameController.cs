3c0ed88b5b456b53b585cdedf0065c5f58cc216c
朝日智宏 <asahii.jr@gmail.com>
2017-11-16 18:48:05 +0900

    山札シャッフルの機能を修正＆追加。

--- Assets/Scripts/Bar04/GameController.cs
+++ Assets/Scripts/Bar04/GameController.cs
@@ -5,8 +5,45 @@ using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Bar0404 {
     public class GameController : MonoBehaviour {
-        public void TransitionToResult() {
+
+        int[] shuffulCards;
+
+        public void TransitionToResult(){
             SceneManager.LoadScene("Result");
         }
+             public void GameStart(){
+
+            //山札をシャッフル
+            shuffulCards = MakeRundumNumber();
+            Debug.Log(shuffulCards[1]);
+            
+
+        }
+
+        //山札をシャッフルするための乱数生成
+        public int[] MakeRundumNumber(){
+            int[] values = new int[53];
+            for (int a = 0; a < 53; a++)
+            {
+                values[a] = a + 1;
+            }
+            for (int i = 0; i < 53; i++)
+            {
+                int abc = UnityEngine.Random.Range(0, values.Length);
+                int xyz;
+                xyz = values[i];
+                values[i] = values[abc];
+                values[abc] = xyz;
+            }
+
+            return values;
+        }
     }
-}
+
+
+        
+       
+    }
+
+
+
--- Assets/Scripts/Bar04/trumpCards_bar0404.cs
+++ Assets/Scripts/Bar04/trumpCards_bar0404.cs
@@ -18,24 +18,6 @@ namespace Assets.Scripts.Bar0404
         }
 
 
-        public int[] shuffleCards()
-        {
-            int[] values = new int[53];
-            for (int a = 0; a < 53; a++)
-            {
-                values[a] = a + 1;
-            }
-            for (int i = 0; i < 53; i++)
-            {
-                int abc = UnityEngine.Random.Range(0, values.Length);
-                int xyz;
-                xyz = values[i];
-                values[i] = values[abc];
-                values[abc] = xyz;
-            }
-
-            return values;
-        }
 
     }
 }
