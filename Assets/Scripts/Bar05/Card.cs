a375262377bc02c094f02fdd2d6b4e26f7f955c1
Higashitani <taiki219hig@gmail.com>
2017-11-16 11:54:50 +0900

    no message

--- Assets/Scripts/Bar05/Card.cs
+++ Assets/Scripts/Bar05/Card.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Card
+namespace Assets.Scripts.Bar05
 {
     public class Card : MonoBehaviour
     {
--- Assets/Scripts/Bar05/HandRank.cs
+++ Assets/Scripts/Bar05/HandRank.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HandRank : MonoBehaviour {
+namespace Assets.Scripts.Bar05
+{
+    public class HandRank : MonoBehaviour
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
 }
