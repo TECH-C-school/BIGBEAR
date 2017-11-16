4e6430f4474573316012921eb9ebf90366dfa934
MikuHorie <kerutinu.lancelot@gmail.com>
2017-11-16 19:32:05 +0900

    背景作成　裏カード表示

--- Assets/Scripts/Bar02/GameController.cs
+++ Assets/Scripts/Bar02/GameController.cs
@@ -8,5 +8,44 @@ namespace Assets.Scripts.Bar02 {
         public void TransitionToResult() {
             SceneManager.LoadScene("Result");
         }
+        private void Start()
+        {
+            CardSet();
+        }
+
+        /// <summary>
+        /// カードを表示
+        /// </summary>
+        private void CardSet()
+        {
+            var cardPrefab = Resources.Load<GameObject>("Prefabs/Bar02/Cards");
+
+
+            int countNumber = 6;
+
+            for (int i = 1; i <= 6; i++)
+            {
+                for (int j = 1; j <= countNumber; j++)
+                {
+                    var cardObject = Instantiate(cardPrefab, transform.position, Quaternion.identity);
+                    cardObject.transform.position = new Vector3(
+                        j - countNumber * 0.5f - 0.5f,
+                        i * 0.5f - 1f,
+                        0);
+                }
+                countNumber--;
+            }
+
+        }
+
+        /// <summary>
+        /// 裏面表示
+        /// </summary>
+        private void TurnCardFaceDown()
+        {
+            Sprite cardSprite = Resources.Load<Sprite>("Images/Bar/Cards/back");
+            var spriteRenderer = transform.GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = cardSprite;
+        }
     }
 }
