d6676e62c84d91d253f8a6e8de62107bd298c1fb
kagoya324 <Rinne.esterica@gmail.com>
2017-11-16 15:35:44 +0900

    表示 ドロー

--- Assets/Scripts/Bar04/GameControll.cs
+++ Assets/Scripts/Bar04/GameControll.cs
@@ -9,10 +9,12 @@ public class GameControll : MonoBehaviour {
 
     [SerializeField][Range(1, 52)]
     public int aaa;
+    int Card_pos = -20;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         CardInput();
+
 
     }
 
@@ -31,17 +33,31 @@ public class GameControll : MonoBehaviour {
         // カードを配る関数
         if (Player_Card.Count < 5)
         {
-            int Card_No = Random.Range(1, Card_List.Count);
-            Debug.Log("CARD:" + Card_No);
-
-            Player_Card.Add(Card_List[Card_No]);
-            Debug.Log(Player_Card[Player_Card.Count -1]);
-            // ここで５枚まで配る
-            // Card_List から Player_Card へ移す
+            Card_pos += 10;
+        
             // GameObjectとしてPrefabからInstantiateして
             // 交換動作の時、 押すとフラグが切り替わる     詳しくはノート書いてある
         }
         
+    }
+
+    public void CardDrow()                 // カードを引く関数
+    {
+        Debug.Log(Card_List.Count);
+
+        int Card_No = Random.Range(0, Card_List.Count - 1);
+
+        Player_Card.Add(Card_List[Card_No]);        // 手札へ
+        Card_List.Remove(Card_List[Card_No]);       // デッキから削除
+        Debug.Log(Card_List.Count);
+        Debug.Log("NO:" + Card_No);
+        Debug.Log("中身:" + Card_List[Card_No]);
+        // ここで５枚まで配る
+        // Card_List から Player_Card へ移す
+
+        GameObject Card_type = Instantiate(Resources.Load("Prefabs/Bar04/Card/" + Card_List[Card_No], typeof(GameObject)), new Vector2(0, 0), Quaternion.identity) as GameObject;
+        Card_type.name = Card_List[Card_No];                                    // 生成時に名前を変えてる
+
     }
 
     void CardInput()
