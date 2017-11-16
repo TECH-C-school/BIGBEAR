98e54aa1b10b0da9eb3c7242e9b7d3f742142c9e
NIYO4490 <ysk44902490@yahoo.co.jp>
2017-11-16 11:56:59 +0900

    新しくscriptsを作りました

---
+++ Assets/Scripts/Bar04/Cards.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cards : MonoBehaviour
+{
+    private int _number;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+}
