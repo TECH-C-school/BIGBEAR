46a22893dd8c7297713ca2e48bea03f68e86c9da
DESKTOP-2MU2PS1\fate0 <fate0720.tb@gmail.com>
2017-11-16 09:07:42 +0900

    シーンを大幅に修正 @高梨

--- Assets/Scripts/Game02/GameController.cs
+++ Assets/Scripts/Game02/GameController.cs
@@ -6,36 +6,37 @@ using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Game02 {
     public class GameController : MonoBehaviour {
-		[SerializeField] Image _scope;
+		[SerializeField] GameObject _scope;
 
 		const float BULLET_SPEED = 900.0f;
 		const float DISTANCE = 1000.0f;
 		const float ACCELERATION = 9.8f;
 
+		private void Start() {
+			_scope.SetActive(false);
+		}
+
 		private void Update() {
 			if(Input.GetMouseButton(0))
 				TouchPoscheck ();
 			if (Input.GetMouseButtonUp(0))
-				_scope.gameObject.SetActive (false);
+				_scope.SetActive (false);
+			
 			#if UNITY_EDITOR
 			if(Input.GetKeyDown("space")){
 
 			}
 			#endif
+
 		}
 
 		private void TouchPoscheck() {
-//			RaycastHit hit;
 			var touchPos = Input.mousePosition;
 			var screenPos = Camera.main.ScreenToWorldPoint (touchPos);
 			screenPos.z = 0;
 			Debug.Log ("Pos:" + screenPos);
-			_scope.gameObject.SetActive (true);
+			_scope.SetActive (true);
 			_scope.gameObject.transform.position= screenPos;
-		}
-
-		private void Snipe(Vector3 firePos){
-
 		}
 
 		public void TransitionToResult() {
---
+++ Assets/Scripts/Game02/Shot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour {
+	[SerializeField] List<GameObject> bulletMagazine;
+
+		public void Snipe(){
+			
+		}
+}
