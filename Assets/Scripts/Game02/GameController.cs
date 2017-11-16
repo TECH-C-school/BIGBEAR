ea612e3a2b80f86d5824f30764d4ece15ab8fff2
W301-01\student <student@tech.com>
2017-11-16 19:09:54 +0900

    現状プッシュ @高梨

--- Assets/Scripts/Game02/GameController.cs
+++ Assets/Scripts/Game02/GameController.cs
@@ -6,37 +6,41 @@ using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Game02 {
     public class GameController : MonoBehaviour {
-		[SerializeField] GameObject _scope;
+		[SerializeField] Shot _scope;
+		SpriteRenderer scopeRenderer {
+			get {
+				return _scope.gameObject.GetComponent<SpriteRenderer> ();
+			}
+		}
 
 		const float BULLET_SPEED = 900.0f;
 		const float DISTANCE = 1000.0f;
 		const float ACCELERATION = 9.8f;
 
 		private void Start() {
-			_scope.SetActive(false);
+			scopeRenderer.enabled = false;
 		}
 
 		private void Update() {
 			if(Input.GetMouseButton(0))
 				TouchPoscheck ();
-			if (Input.GetMouseButtonUp(0))
-				_scope.SetActive (false);
-			
-			#if UNITY_EDITOR
-			if(Input.GetKeyDown("space")){
-
-			}
-			#endif
+			if (Input.GetMouseButtonUp (0))
+				scopeRenderer.enabled = false;
 
 		}
 
 		private void TouchPoscheck() {
 			var touchPos = Input.mousePosition;
 			var screenPos = Camera.main.ScreenToWorldPoint (touchPos);
-			screenPos.z = 0;
-			Debug.Log ("Pos:" + screenPos);
-			_scope.SetActive (true);
+			screenPos.z = -0.5f;
+//			Debug.Log ("Pos:" + screenPos);
+			scopeRenderer.enabled = true;
 			_scope.gameObject.transform.position= screenPos;
+			#if UNITY_EDITOR
+			if(Input.GetKey("space")){
+				_scope.Snipe();
+			}
+			#endif
 		}
 
 		public void TransitionToResult() {
--- Assets/Scripts/Game02/Shot.cs
+++ Assets/Scripts/Game02/Shot.cs
@@ -5,7 +5,20 @@ using UnityEngine;
 public class Shot : MonoBehaviour {
 	[SerializeField] List<GameObject> bulletMagazine;
 
-		public void Snipe(){
-			
+	Transform scopeTransform {
+		get {
+			return this.gameObject.transform;
 		}
+	}
+
+	public void Snipe(){
+		var ray = new Ray (scopeTransform.position, scopeTransform.forward);
+		RaycastHit rHit;
+		if(Physics.Raycast(ray, out rHit)){
+			if(rHit.collider.tag == "Enemy"){
+				Debug.Log ("Hit!!!!!!");
+			}
+		}
+		Debug.DrawRay (ray.origin, ray.direction, Color.red);
+	}
 }
