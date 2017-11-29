ef4b63311400e4790f0d24f0383263f91f8262fe
Hirokawa <17d3170021hm@edu.tech.ac.jp>
2017-11-29 15:20:21 +0900

    自分の手札を表面にした

---
+++ Assets/Scripts/Bar05/Card.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+
+    //ナンバーを持ってきてセットする
+    private int _number;
+
+    public int Number
+    {
+        get { return _number; }
+        set { _number = value; }
+    }
+
+    //カードのナンバー管理beta版
+    string[] Cards1 = { "c01", "c02", "c03", "c04", "c05", "c06", "c07", "c08", "c09", "c10", "c11", "c12", "c13", };
+    string[] Cards2 = { "d01", "d02", "d03", "d04", "d05", "d06", "d07", "d08", "d09", "d10", "d11", "d12", "d13", };
+    string[] Cards3 = { "h01", "h02", "h03", "h04", "h05", "h06", "h07", "h08", "h09", "h10", "h11", "h12", "h13", };
+    string[] Cards4 = { "s01", "s02", "s03", "s04", "s05", "s06", "s07", "s08", "s09", "s10", "s11", "s12", "s13", };
+
+    /// <summary>
+    /// 割り当てられたナンバーをCardKeyに変換
+    /// </summary>
+    /// <returns></returns>
+    string cardtype()
+    {
+        //カードのマーク
+        double cardMark = _number / 13 + 1;
+        cardMark = Math.Floor(cardMark);
+        //カードの数字
+        int cardNumber = _number % 13;
+        //入れ物作って
+        string CardKey = null;
+        //調べて
+        if (cardMark == 1)
+        {
+            CardKey = Cards1[cardNumber];
+        }
+
+        else if (cardMark == 2)
+        {
+            CardKey = Cards2[cardNumber];
+        }
+
+        else if (cardMark == 3)
+        {
+            CardKey = Cards3[cardNumber];
+        }
+
+        else if (cardMark == 4)
+        {
+            CardKey = Cards4[cardNumber];
+        }
+
+        else
+        {
+            Debug.Log("Cardの読み込みができませんでした");
+        }
+
+        //どーん(?)
+        return CardKey;
+    }
+
+    /// <summary>
+    /// カードを表示するよ
+    /// </summary>
+    public void CardMake()
+    {
+        Sprite cardSprite = null;
+        cardSprite = Resources.Load<Sprite>("Images/Bar/Cards/" + cardtype());
+        var spriteRenderer = transform.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = cardSprite;
+        Debug.Log(cardSprite);
+    }
+
+    /// <summary>
+    /// カードのナンバー管理のつもりだった
+    /// もしかして --> もういらない
+    /// </summary>
+    private enum CardsNum
+    {
+        c01 = 0, c02, c03, c04, c05, c06, c07, c08, c09, c10, c11, c12, c13,
+        d01, d02, d03, d04, d05, d06, d07, d08, d09, d10, d11, d12, d13,
+        h01, h02, h03, h04, h05, h06, h07, h08, h09, h10, h11, h12, h13,
+        s01, s02, s03, s04, s05, s06, s07, s08, s09, s10, s11, s12, s13,
+    }
+
+}
--- Assets/Scripts/Bar05/GameController.cs
+++ Assets/Scripts/Bar05/GameController.cs
@@ -8,8 +8,11 @@ namespace Assets.Scripts.Bar05 {
 
         private void Start()
         {
-                MakeCards();
+            MakeCards();
+            
         }
+
+        //カード周りここから
 
         /// <summary>
         /// 自分と相手のカードを配置する
@@ -18,7 +21,13 @@ namespace Assets.Scripts.Bar05 {
         /// </summary>
         public void MakeCards()
         {
-            Debug.Log("Load to MakeCards");
+            //山札を作る
+            int deckPlace = 0;
+            int[] Deck = Cardsshuffle();
+
+            //デバッグ用
+            //for(int i = 0; i < Deck.Length; i++){Debug.Log(Deck[i]);} Debug.Log("--------------------");
+
             //PrefabをLoadする
             var cardPrefab = Resources.Load<GameObject>("Prefabs/Bar05/Card");
             for (int i = 0; i < 2; i++)
@@ -35,6 +44,11 @@ namespace Assets.Scripts.Bar05 {
                         cardObject.transform.position = new Vector3((x * 2 - 1), -3.4f, 0);
                         //親を指定
                         cardObject.transform.parent = cardsObject1.transform;
+                        //カードのナンバー指定
+                        var card = cardObject.GetComponent<Card>();
+                        card.Number = Deck[deckPlace];
+                        card.CardMake();
+                        deckPlace++;
                         Debug.Log("<color=green>自分のカード生成</color>" + (x + 1) + "枚目");
                     }
                     Debug.Log("<color=blue>自分のカード生成完了</color>");
@@ -59,14 +73,101 @@ namespace Assets.Scripts.Bar05 {
         /// </summary>
         private enum CardsNum
         {
-            c01 = 1, c02, c03, c04, c05, c06, c07, c08, c09, c10, c11, c12, c13,
+            c01 = 0, c02, c03, c04, c05, c06, c07, c08, c09, c10, c11, c12, c13,
             d01, d02, d03, d04, d05, d06, d07, d08, d09, d10, d11, d12, d13,
             h01, h02, h03, h04, h05, h06, h07, h08, h09, h10, h11, h12, h13,
             s01, s02, s03, s04, s05, s06, s07, s08, s09, s10, s11, s12, s13,
         }
+
+        /// <summary>
+        /// カードをシャッフルする
+        /// </summary>
+        /// <returns></returns>
+        private int[] Cardsshuffle()
+        {
+            int[] decks = new int[52];
+            for(int i = 0; i < 52; i++)
+            {
+                decks[i] = i;
+            }
+
+            int counter = 0;
+            while(counter < 52)
+            {
+                int index = UnityEngine.Random.Range(counter, decks.Length);
+                int tmp = decks[counter];
+                decks[counter] = decks[index];
+                decks[index] = tmp;
+                counter++;
+            }
+            return decks;
+        }
+
+        //カード周りここまで
+
+        //ゲーム進行ここから
+
+        private enum GamePhase
+        {
+            FirstBet = 0,
+            SecondBet,
+            ThirdBet,
+            FinalBet,
+            Result,
+        }
+        
+        //ゲーム進行ここまで
 
         public void TransitionToResult() {
             SceneManager.LoadScene("Result");
         }
     }
 }
+
+/*
+ * 判定アルゴリズム雑記
+ * 
+ * ルールはご存知の通り手札+場札の7枚の内最適な5枚の役で勝負する
+ * ではどうやってその役を見分けるか
+ * 
+ * 役一覧(昇順)
+ * ハイカード(ノーペア)
+ * ワンペア
+ * ツーペア
+ * スリーカード
+ * ストレート
+ * フラッシュ
+ * フルハウス
+ * フォーカード
+ * ストレートフラッシュ
+ * ロイヤルストレートフラッシュ
+ * 
+ * 同じ数字が揃わない役はハイカード・ストレート・フラッシュ、ストレートフラッシュ、ロイヤルストレートフラッシュ
+ * 裏を返せば他の役は同じ数字が揃うので、最初はペアの有無を見て判定を進める
+ * 
+ * ペアある組
+ * ワンペア
+ * ツーペア
+ * スリーカード
+ * フルハウス
+ * フォーカード
+ * 
+ * まずはフォーカードの判定 (Y = フォーカード)
+ * 次にスリーカードがあるかを判定し、ある場合更にペアが無いかを探す(Y = フルハウス;N =スリーカード)
+ * スリーカードが無かった場合、最初のペア以外にペアがあるかを探す(Y = ツーペア;N =ワンペア)
+ * 
+ * ペアない組
+ * ハイカード
+ * ストレート
+ * フラッシュ
+ * ストレートフラッシュ
+ * ロイヤルストレートフラッシュ
+ * 
+ * ペアがあった場合でも判定は通す
+ * まずはロイヤルストレートフラッシュのみあるか判定(Y = ロイヤルストレートフラッシュ)
+ * 次にフラッシュの判定
+ * あったらそれがストレートがどうかを判定(Y = ストレートフラッシュ;N = フラッシュ)
+ * もう一回ストレートの判定(Y = ストレート; N = ハイカード)
+ * 両方無かったらハイカード確定
+ * 多分な
+ */
