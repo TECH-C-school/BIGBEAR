1e6f54fc804707b0f20700591379be2e915a59f4
NIYO449 <ysk44902490@yahoo.co.jp>
2017-11-29 18:55:19 +0900

    scriptを弄りました

--- Assets/Scripts/Bar04/Cards.cs
+++ Assets/Scripts/Bar04/Cards.cs
@@ -1,13 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cards : MonoBehaviour
 {
-    private int _number;
-    // Use this for initialization
-    void Start()
+    //列挙型...トランプの種類
+    public enum PalayingCards{
+        s,
+        h,
+        d,
+        c,
+
+        }
+
+    //トランプの種類
+    public PalayingCards CardType;
+    //トランプの数
+    public int Number;
+    //トランプの裏面を入れる箱
+    public Sprite Back;
+    //トランプの表面を入れる箱
+    public Sprite Front;
+
+    public Cards(int number,PalayingCards s)
     {
+        Number = number;
+        CardType = s;
+        
+    }
+
+    private void Start()
+    {
+        //C# tostring 書式で調べる
+        string path = CardType.ToString() + Number.ToString("d2");
+        Debug.Log(path);
 
     }
+
 }
+
--- Assets/Scripts/Bar04/GameController.cs
+++ Assets/Scripts/Bar04/GameController.cs
@@ -5,6 +5,17 @@ using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Bar04 {
     public class GameController : MonoBehaviour {
+
+        
+
+        private void Start()
+        {
+            Cards card = new Cards(1,Cards.PalayingCards.s);
+
+            Debug.Log("カード生成");
+            Debug.Log(card.Number);
+            Debug.Log(card.CardType);
+        }
         public void TransitionToResult() {
             SceneManager.LoadScene("Result");
         }
