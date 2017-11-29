412eeee610b7e242bcbff39de410ed074a209196
松田幸介 <darby8928@gmail.com>
2017-11-29 15:30:30 +0900

    ホーム画面の追加、列挙型の作成

--- Assets/Scripts/Bar04/GameController.cs
+++ Assets/Scripts/Bar04/GameController.cs
@@ -5,8 +5,36 @@ using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Bar04 {
     public class GameController : MonoBehaviour {
+        public enum Number{
+           one,
+           twe,
+           three,
+           four,
+           five,
+           six,
+           seven,
+           eigth,
+           nighn,
+           ten,
+           J,
+           Q,
+           K,
+           joker
+        }
+        public enum Mark{
+            clover,
+            diamond,
+            heart,
+            spade
+        }
+        
+        
+
+
+
         public void TransitionToResult() {
             SceneManager.LoadScene("Result");
         }
+
     }
 }
