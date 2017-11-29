581a0fa86c4cadfc41ffda7b7a752c211a5cba3b
sigkarla888 <sigkalra8888@gmail.com>
2017-11-29 18:52:05 +0900

    背景の変更、UIの追加、スクリプトの追加

---
+++ Assets/Scripts/Bar06/CardMaster.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardMaster : MonoBehaviour
+{
+    void Start()
+    {
+        MakeCard();
+    }
+
+    public void MakeCard()
+    {
+        GameObject[] cards = GameObject.FindGameObjectsWithTag("CardObjects");
+
+        Sprite[] sprites = Resources.LoadAll<Sprite>("Images/Cards");
+
+        foreach(GameObject cds in cards)
+        {
+            SpriteRenderer cdsObRender = cds.GetComponent<SpriteRenderer>();
+
+            int[] textureNum = cds.GetComponent<CardNum_und_Mark>().setCard;
+            int[] textureNum2 = cds.GetComponent<CardNum_und_Mark>().setCard2;
+
+            for (int i = 0; i < 2; i++)
+            {
+                switch (textureNum[i])
+                {
+                    case 1:
+                        if (textureNum2[i] == 1)
+                        {
+                            cdsObRender.sprite = sprites[1];
+                        }
+                        else if (textureNum2[i] == 2)
+                        {
+                            cdsObRender.sprite = sprites[14];
+                        }
+                        else if (textureNum2[i] == 3)
+                        {
+                            cdsObRender.sprite = sprites[27];
+                        }
+                        else
+                        {
+                            cdsObRender.sprite = sprites[41];
+                        }
+                        break;
+
+                    case 2:
+                        if (textureNum2[i] == 1)
+                        {
+                            cdsObRender.sprite = sprites[2];
+                        }
+                        else if (textureNum2[i] == 2)
+                        {
+                            cdsObRender.sprite = sprites[15];
+                        }
+                        else if (textureNum2[i] == 3)
+                        {
+                            cdsObRender.sprite = sprites[28];
+                        }
+                        else
+                        {
+                            cdsObRender.sprite = sprites[42];
+                        }
+                        break;
+
+                    case 3:
+                        if (textureNum2[i] == 1)
+                        {
+                            cdsObRender.sprite = sprites[3];
+                        }
+                        else if (textureNum2[i] == 2)
+                        {
+                            cdsObRender.sprite = sprites[16];
+                        }
+                        else if (textureNum2[i] == 3)
+                        {
+                            cdsObRender.sprite = sprites[29];
+                        }
+                        else
+                        {
+                            cdsObRender.sprite = sprites[43];
+                        }
+                        break;
+
+                    case 4:
+                        if (textureNum2[i] == 1)
+                        {
+                            cdsObRender.sprite = sprites[4];
+                        }
+                        else if (textureNum2[i] == 2)
+                        {
+                            cdsObRender.sprite = sprites[17];
+                        }
+                        else if (textureNum2[i] == 3)
+                        {
+                            cdsObRender.sprite = sprites[30];
+                        }
+                        else
+                        {
+                            cdsObRender.sprite = sprites[44];
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Bar06/CardNum_und_Mark.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardNum_und_Mark : MonoBehaviour {
+
+    public int[] setCard = new int[2];
+    public int[] setCard2 = new int[2];
+
+    void Start()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            int setNum = Random.Range(1, 14);
+            int setMark = Random.Range(1, 5);
+            setCard[i] = setNum;
+            setCard2[i] = setMark;
+        }
+    }
+         
+}
