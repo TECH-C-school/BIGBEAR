b03c32fce5066c7cf7b0e500efa392d16ed51729
sample <sample@sample.com>
2017-11-29 15:31:14 +0900

    カードシャッフル

--- Assets/Scripts/Bar01/Bar01_04.cs
+++ Assets/Scripts/Bar01/Bar01_04.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 public class Bar01_04 : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-       
-	}
+	void Start (){
+        //シャッフルする配列
+        int[] ary = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        //Fisher-Yatesアルゴリズムでシャッフルする
+        System.Random rng = new System.Random();
+        int n = ary.Length;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            int tmp = ary[k];
+            ary[k] = ary[n];
+            ary[n] = tmp;
+            Debug.Log(k);      
+        }        
+    }
+
 	
 	// Update is called once per frame
 	void Update () {
