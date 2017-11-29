23da9bb611b9bfae96606c6d1f304be1f0189257
Higashitani <taiki219hig@gmail.com>
2017-11-29 18:54:07 +0900

    フェイズスクリプト生成中

--- Assets/Scripts/Bar05/Card.cs
+++ Assets/Scripts/Bar05/Card.cs
@@ -6,6 +6,8 @@ namespace Assets.Scripts.Bar05
 {
     public class Card : MonoBehaviour
     {
+        public int number;
+
         public enum Suit
         {
             Spade,
@@ -17,28 +19,24 @@ namespace Assets.Scripts.Bar05
 
         public Suit suit;
 
-        public int number;
-
-        public string[] CardStr =
-        {
-            "SA", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "SJ", "SQ", "SK",
-        "HA", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "HJ", "HQ", "HK",
-        "DA", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "DJ", "DQ", "DK",
-        "CA", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "CJ", "CQ", "CK",
-        "J0", "J1"
-        };
 
         void Start()
         {
-            GameObject card = (GameObject)Resources.Load("Prefab/Bar_05");
+            GameObject card = (GameObject)Resources.Load("Prefabs/Bar_05/back");
             var Card = GameObject.Find("Card");
-            GameObject[] cards = new GameObject[53];
         }
 
         // Update is called once per frame
         void Update()
         {
 
+        }
+
+        string CardInfo(string cardInfo)
+        {
+            string card = ""+ cardInfo;
+
+            return card;
         }
     }
 }
---
+++ Assets/Scripts/Bar05/Phase.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Phase : MonoBehaviour {
+
+    public GameObject card;
+
+    public enum PhaseEnum
+    {
+        プリフロップ,
+        フロップ,
+        ターン,
+        リバー,
+        ショーダウン
+    }
+
+    public PhaseEnum phase;
+
+
+    public string[] CardStr =
+    {
+            "s01", "s02", "s03", "s04", "s05", "s06", "s07", "s08", "s09", "s10", "s11", "s12", "s13",
+        "h01", "h02", "h03", "h04", "h05", "h06", "h07", "h08", "h09", "h10", "h11", "h12", "h13",
+        "d01", "d02", "d03", "d04", "d05", "d06", "d07", "d08", "d09", "d10", "d11", "d12", "d13",
+        "c01", "c02", "c03", "c04", "c05", "c06", "c07", "c08", "c09", "c10", "c11", "c12", "c13",
+
+        };
+
+    void Start()
+    {
+        card = Resources.Load("Prefabs/Bar05/back") as GameObject;
+    }
+
+	void Update ()
+    {
+        if (phase == PhaseEnum.プリフロップ)
+        {
+            
+        }
+	}
+
+    private void MakeCard()
+    {
+        int[] randomNumbers = MakeRandomNumbers();
+
+        var cardPrefab = Resources.Load<GameObject>("Prefab/Card");
+        var BoardCard = new GameObject("Board");
+        var HandCard = new GameObject("Hand");
+        var EnemyHand = new GameObject("EnemyHand");
+
+        for (int i = 0;i < 2; i++)
+        {
+            
+            {
+                var cardObject = Instantiate(cardPrefab,transform.position,Quaternion.identity);
+
+                cardObject.transform.position = new Vector3();
+
+                //cardObject.transform.parent = CreateCard.transform;
+            }
+
+
+        }
+        
+    }
+
+    public int[] MakeRandomNumbers()
+    {
+        int[] numbers = new int[52];
+        return numbers;
+    }
+
+    void CreateCard()
+    {
+        
+    }
+
+    void handDeal()
+    {
+        
+    }
+
+}
