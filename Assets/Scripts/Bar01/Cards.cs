0283cfe6700f421c2148288ad315b5496d9c5df2
DESKTOP-CLISS3A\s_u_k <suzuki527@docomo.ne.jp>
2017-11-29 15:33:35 +0900

    カードの表示のスクリプト編集

---
+++ Assets/Scripts/Bar01/Cards.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cards : MonoBehaviour
+{
+    class Makecard
+    {
+        static public int[] c = new int[13];
+        public int[] d = new int[13];
+        public int[] h = new int[13];
+        public int[] s = new int[13];
+    }
+
+    void Start()
+    {
+        var cardsObject = GameObject.Find("back");
+        cardsObject.transform.position = new Vector3(0, 0, 0);
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    int[] MakeRandomNumber()
+    {
+        int[] number = new int[13];
+        for (int i = 0; i < 13; i++)
+        {
+            number[i] = i + 1;
+        }
+        int count = 0;
+        while ( count < 13)
+        {
+            var index = Random.Range(count, number.Length);
+            var tmp = number[count];
+            number[count] = number[index];
+            number[index] = tmp;
+
+            count++;
+        }
+        return number;
+    }
+}
