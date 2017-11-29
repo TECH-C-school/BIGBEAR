af3636982328d99aba75bf836edfdb3e5f3030e1
KimitaSora <minecraft.nichijo@ezweb.ne.jp>
2017-11-29 15:36:11 +0900

    Cards、GameControllerの編集

--- Assets/Scripts/Bar03/Cards.cs
+++ Assets/Scripts/Bar03/Cards.cs
@@ -4,4 +4,44 @@ using UnityEngine;
 
 public class Cards : MonoBehaviour
 {
+
+    private int _number;
+    private string _string;
+    public int Number
+    {
+        get { return _number; }
+        set { _number = value; }
+    }
+    public string String
+    {
+        get { return _string; }
+        set { _string = value; }
+    }
+
+    //publicはGameControllerから呼び出せる
+    public void TurnCardFaceUp()
+    {
+        TurnCard(true);
+    }
+
+    public void TurnCardFaceDown()
+    {
+        TurnCard(false);
+    }
+    //privateは呼び出せない
+    private void TurnCard(bool faceUp)
+    {
+        Sprite cardSprite = null;
+
+        if (faceUp)
+        {
+            cardSprite = Resources.Load<Sprite>(_string);
+        }
+        else
+        {
+            cardSprite = Resources.Load<Sprite>("Images/Bar/Cards/back");
+        }
+        var spriteRenderer = transform.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = cardSprite;
+        }
 }
--- Assets/Scripts/Bar03/GameController.cs
+++ Assets/Scripts/Bar03/GameController.cs
@@ -6,10 +6,50 @@ using UnityEngine.SceneManagement;
 namespace Assets.Scripts.Bar03 {
     public class GameController : MonoBehaviour
     {
+        
+        private string[] cardMark = new string[104] 
+        {"Images/Bar/Cards/c01","Images/Bar/Cards/c02","Images/Bar/Cards/c03",
+        "Images/Bar/Cards/c04","Images/Bar/Cards/c05","Images/Bar/Cards/c06",
+        "Images/Bar/Cards/c07","Images/Bar/Cards/c08","Images/Bar/Cards/c09",
+        "Images/Bar/Cards/c10","Images/Bar/Cards/c11","Images/Bar/Cards/c12",
+        "Images/Bar/Cards/c13","Images/Bar/Cards/c01","Images/Bar/Cards/c02",
+        "Images/Bar/Cards/c03","Images/Bar/Cards/c04","Images/Bar/Cards/c05",
+        "Images/Bar/Cards/c06","Images/Bar/Cards/c07","Images/Bar/Cards/c08",
+        "Images/Bar/Cards/c09","Images/Bar/Cards/c10","Images/Bar/Cards/c11",
+        "Images/Bar/Cards/c12","Images/Bar/Cards/c13",
+        "Images/Bar/Cards/d01","Images/Bar/Cards/d02","Images/Bar/Cards/d03",
+        "Images/Bar/Cards/d04","Images/Bar/Cards/d05","Images/Bar/Cards/d06",
+        "Images/Bar/Cards/d07","Images/Bar/Cards/d08","Images/Bar/Cards/d09",
+        "Images/Bar/Cards/d10","Images/Bar/Cards/d11","Images/Bar/Cards/d12",
+        "Images/Bar/Cards/d13","Images/Bar/Cards/d01","Images/Bar/Cards/d02",
+        "Images/Bar/Cards/d03","Images/Bar/Cards/d04","Images/Bar/Cards/d05",
+        "Images/Bar/Cards/d06","Images/Bar/Cards/d07","Images/Bar/Cards/d08",
+        "Images/Bar/Cards/d09","Images/Bar/Cards/d10","Images/Bar/Cards/d11",
+        "Images/Bar/Cards/d12","Images/Bar/Cards/d13",                  
+        "Images/Bar/Cards/h01","Images/Bar/Cards/h02","Images/Bar/Cards/h03",
+        "Images/Bar/Cards/h04","Images/Bar/Cards/h05","Images/Bar/Cards/h06",
+        "Images/Bar/Cards/h07","Images/Bar/Cards/h08","Images/Bar/Cards/h09",
+        "Images/Bar/Cards/h10","Images/Bar/Cards/h11","Images/Bar/Cards/h12",
+        "Images/Bar/Cards/h13","Images/Bar/Cards/h01","Images/Bar/Cards/h02",
+        "Images/Bar/Cards/h03","Images/Bar/Cards/h04","Images/Bar/Cards/h05",
+        "Images/Bar/Cards/h06","Images/Bar/Cards/h07","Images/Bar/Cards/h08",
+        "Images/Bar/Cards/h09","Images/Bar/Cards/h10","Images/Bar/Cards/h11",
+        "Images/Bar/Cards/h12","Images/Bar/Cards/h13",
+        "Images/Bar/Cards/s01","Images/Bar/Cards/s02","Images/Bar/Cards/s03",
+        "Images/Bar/Cards/s04","Images/Bar/Cards/s05","Images/Bar/Cards/s06",
+        "Images/Bar/Cards/s07","Images/Bar/Cards/s08","Images/Bar/Cards/s09",
+        "Images/Bar/Cards/s10","Images/Bar/Cards/s11","Images/Bar/Cards/s12",
+        "Images/Bar/Cards/s13","Images/Bar/Cards/s01","Images/Bar/Cards/s02",
+        "Images/Bar/Cards/s03","Images/Bar/Cards/s04","Images/Bar/Cards/s05",
+        "Images/Bar/Cards/s06","Images/Bar/Cards/s07","Images/Bar/Cards/s08",
+        "Images/Bar/Cards/s09","Images/Bar/Cards/s10","Images/Bar/Cards/s11",
+        "Images/Bar/Cards/s12","Images/Bar/Cards/s13"} ;
         void Start()
         {
             MakeBackCards();
             BackGroundMake();
+            
+            
         }
         public void TransitionToResult()
         {
@@ -17,6 +57,9 @@ namespace Assets.Scripts.Bar03 {
         }
         private void MakeBackCards()
         {
+            int count = 0;
+            int[] randomNumbers = MakeRandomNumbers();
+
             Transform parentObject = GameObject.Find("Cards").transform;
             GameObject cardPrefabs = Resources.Load<GameObject>("Prefabs/Bar03/Back");
             for (int x = 0; x < 10; x++)
@@ -30,6 +73,10 @@ namespace Assets.Scripts.Bar03 {
                         y * 0.31f + 2.06f,
                         0);
                     cardObject.transform.parent = parentObject;
+
+                    Cards cardSet = cardObject.GetComponent<Cards>();
+                    
+                    count++;
                 }
             }
         }
@@ -56,6 +103,26 @@ namespace Assets.Scripts.Bar03 {
                     DownCardFlameObject.transform.parent = parentObject2;
                 }
             }
+        }
+        private int[] MakeRandomNumbers()
+        {
+            int[] values = new int[104];
+            for (int n = 0; n < 104; n++)
+            {
+                values[n] += n + 1;
+            }
+            var counter = 0;
+            while (counter < 104)
+            {
+                var index = Random.Range(counter, values.Length);
+                var tmp = values[counter];
+                values[counter] = values[index];
+                values[index] = tmp;
+
+                counter++;
+            }
+            
+            return values;
         }
     }
         
