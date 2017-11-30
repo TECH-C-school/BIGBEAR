2271b1d22918778d10713b5c3b356bb71927d881
Tomioka <tyuuni31@gmail.com>
2017-11-30 11:59:44 +0900

    Script追加

--- Assets/Scripts/Bar06/Cards.cs
+++ Assets/Scripts/Bar06/Cards.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class Cards : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+
+        //数字とマークの表示
+        string[] mark = new string[] { "S", "C", "H", "D" };
+        for (int i = 1; i <= 52; i++)
+        {
+            var rndnumber = Random.Range(1, 14);
+            var rndMark = Random.Range(0, 4);
+
+            Debug.Log(mark[rndMark] + rndnumber);
+        }
+    }
+
+
+
+
+
+    // Update is called once per frame
+
+    public GameObject c;
+    void Update() {
+        c = GameObject.Find("C12");
+    }
 }
