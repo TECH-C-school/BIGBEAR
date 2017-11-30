2c26a5e49a4da569e8bca77b2becf686358fe321
松田幸介 <darby8928@gmail.com>
2017-11-30 17:28:50 +0900

    scriptsの更新

--- Assets/Scripts/Bar04/GameController.cs
+++ Assets/Scripts/Bar04/GameController.cs
@@ -5,31 +5,50 @@ using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Bar04 {
     public class GameController : MonoBehaviour {
-        public enum Number{
-           one,
-           twe,
-           three,
-           four,
-           five,
-           six,
-           seven,
-           eigth,
-           nighn,
-           ten,
-           J,
-           Q,
-           K,
-           joker
+        public enum Mark
+        {
+            Spade,
+            Haert,
+            Diamonds,
+            Club,
+            Joker
         }
-        public enum Mark{
-            clover,
-            diamond,
-            heart,
-            spade
+        public enum Cards
+        {
+            one,
+            twe,
+            three,
+            hour,
+            five,
+            six,
+            seven,
+            eight,
+            nighn,
+            ten,
+            Jack,
+            queen,
+            king
         }
-        
-        
+        void Start()
+        {
 
+            var cardPrefabs = Resources.Load<GameObject>("Prefabs/Bar04/card");
+            for (var i = 0; i < 5; i++)
+            {
+                var cardObbject = Instantiate(cardPrefabs, transform.position, Quaternion.identity);
+                cardObbject.transform.position = new Vector3(i * 2.5f - 5, 0.5f, 0);
+            }
+        }
+        void Update()
+        {
+            MakeRandomCards();
+        }
+
+        private int MakeRandomCards()
+        {
+            int Num = UnityEngine.Random.Range(0, 13);
+            return Num;
+        }
 
 
         public void TransitionToResult() {
