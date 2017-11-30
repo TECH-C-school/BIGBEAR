745512c1294839dd0e220079293f05992c4c91f5
市橋 優都 <yuto.184@icloud.com>
2017-11-30 12:00:57 +0900

    トランプ生成途中

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        string[] mark = new string[] { "D", "S", "H", "C" };
+        for (int i = 0; i <= 52; i++){
+                
+        }
+
+    }
+}
