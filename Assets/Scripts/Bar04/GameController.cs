df22cffd12acd361ef786b5f08d7cc4422dac3e2
松田幸介 <darby8928@gmail.com>
2017-11-30 18:41:17 +0900

    ランダムなマークと数字の処理を追加

--- Assets/Scripts/Bar04/GameController.cs
+++ Assets/Scripts/Bar04/GameController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -38,17 +39,43 @@ namespace Assets.Scripts.Bar04 {
                 var cardObbject = Instantiate(cardPrefabs, transform.position, Quaternion.identity);
                 cardObbject.transform.position = new Vector3(i * 2.5f - 5, 0.5f, 0);
             }
+            var FightButton = GameObject.Find("b_s");
+            FightButton.transform.position = new Vector3(6.5f,-3.5f,-1);
         }
+        
         void Update()
         {
-            MakeRandomCards();
+            ClickFight();
+            
         }
 
-        private int MakeRandomCards()
+        private int MakeRandomNumbers()
         {
             int Num = UnityEngine.Random.Range(0, 13);
             return Num;
         }
+        private int MakeRandomMark()
+        {
+            int Num = UnityEngine.Random.Range(0, 4);
+            return Num;
+        }
+        private void returnCard()
+        {
+
+        }
+        private void ClickFight()
+        {
+            if (Input.GetMouseButtonUp(0)) return;
+
+            var tapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (!Physics2D.OverlapPoint(tapPoint)) return;
+
+            var hitObject = Physics2D.Raycast(tapPoint, -Vector2.up);
+            if (!hitObject) return;
+
+        }
+
 
 
         public void TransitionToResult() {
