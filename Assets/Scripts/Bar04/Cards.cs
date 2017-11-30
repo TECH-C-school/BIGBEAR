24fd7ba4d8a5cc6210d0415c7ebe88dd41c65d93
MakotoAoki <aomako9923@gmail.com>
2017-11-30 11:26:09 +0900

    カード表示

---
+++ Assets/Scripts/Bar04/Cards.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cards : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		MakeCards();
+	}
+
+	private void MakeCards()
+    {
+        var cardprefab = Resources.Load<GameObject>("Prefabs/Bar04/Card");
+
+        for (int i = 0; i < 5; i++)
+        {
+            var cardObject = Instantiate(gameObject, transform.position, Quaternion.identity);
+        }
+    }
+		
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
