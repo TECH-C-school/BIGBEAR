fafb28e6ea825c837c9a303ab9ecba07e533c5df
Hara Kazunori <rize.game.0816@gmail.com>
2017-11-30 12:00:09 +0900

    背景、スクリプトの編集

--- Assets/Scripts/Bar06/GameController.cs
+++ Assets/Scripts/Bar06/GameController.cs
@@ -5,12 +5,28 @@ using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Bar06 {
     public class GameController : MonoBehaviour {
-        private void MakeCard()
-        {
-           
-        }
         public void TransitionToResult() {
             SceneManager.LoadScene("Result");
+        }
+
+        private void card()
+        {
+            int number = 0;
+            string number2 = "";
+            string[] mark = new string[] { "h", "d", "s", "c"};
+            for (int i = 0; i <= 52; i++)
+            {
+                if (i >= 51)
+                {
+                    number2 = "";
+                } else
+                {
+                    number = i % 13;
+                    number2 = number.ToString();
+                }
+
+                Debug.Log(mark[i / 13] + number2);
+            }
         }
     }
 }
