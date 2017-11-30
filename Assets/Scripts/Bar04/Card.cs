365e9801f8ea4aa4c136fa1076193b1b3618f1d9
Naoya Fujita <nao8yan@gmail.com>
2017-11-30 12:10:00 +0900

    スクリプト記入

---
+++ Assets/Scripts/Bar04/Card.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+
+    private LayerMask mask;
+
+    // Use this for initialization
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    //void OnMouseDown()
+    //{
+
+
+    //    Debug.Log("hogehoge");
+
+    //}
+
+    //void ray()
+    //{
+    //    var Sel = gameObject.transform.FindChild("SelCard").gameObject;
+    //    // Rayの作成
+    //    Ray ray = new Ray(transform.position, transform.forward);
+    //    Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+    //    // Rayが衝突したコライダーの情報を得る
+    //    RaycastHit hit;
+
+    //    // Rayが衝突したかどうか
+    //    if (Physics.Raycast(ray,out hit, 10.0f, mask))
+    //    {
+    //        var gae = hit.collider.GetComponentInChildren<GameObject>();
+    //        gae.
+
+    //    }
+    //    //clickされた奴の子を非表示にする作業中。
+
+    //}
+
+
+
+
+
+
+
+
+    /*
+        //スマホのタッチ判定
+        //スマホ向け そのオブジェクトがタッチされていたらtrue（マルチタップ対応）
+        bool OneTouchDown()
+        {
+            // タッチされているとき
+            if (0 < Input.touchCount)
+            {
+                // タッチされている指の数だけ処理
+                for (int i = 0; i < Input.touchCount; i++)
+                {
+                    // タッチ情報をコピー
+                    Touch t = Input.GetTouch(i);
+                    // タッチしたときかどうか
+                    if (t.phase == TouchPhase.Began)
+                    {
+                        //タッチした位置からRayを飛ばす
+                        Ray ray = Camera.main.ScreenPointToRay(t.position);
+                        RaycastHit hit = new RaycastHit();
+                        if (Physics.Raycast(ray, out hit))
+                        {
+                            //Rayを飛ばしてあたったオブジェクトが自分自身だったら
+                            if (hit.collider.gameObject == cardPrefab)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;//タッチされてなかったらfalse
+        }
+        */
+
+
+
+
+
+
+
+
+
+}
+
--- Assets/Scripts/Bar04/GameController.cs
+++ Assets/Scripts/Bar04/GameController.cs
@@ -3,10 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
+
+
 namespace Assets.Scripts.Bar04 {
     public class GameController : MonoBehaviour {
         public void TransitionToResult() {
             SceneManager.LoadScene("Result");
         }
-    }
+
+
+
+
+      [SerializeField]
+      private GameObject cardPrefab;
+      [SerializeField]
+      private GameObject cardSelect;
+
+
+
+
+        void Start()
+{
+    MakeCards();
+
+}
+//カード複製 or 配置
+private void MakeCards()
+{
+    //カードを５枚複製する
+    for (int i = 0; i < 5; i++)
+    {
+        //カードを並べる
+        var cardObject = Instantiate(cardPrefab, transform.position, Quaternion.identity);
+        cardObject.transform.position = new Vector2(
+                i * 1.8f - 3.84f, 0);
+
+        //カード生成
+        GameObject SelCard = Instantiate(cardSelect);
+        SelCard.SetActive(false);
+
+        //親子付け
+        SelCard.transform.parent = cardObject.transform;
+
+
+        //黄色い枠の位置の設定　　　　カードの位置　+　z軸を-1する。
+        Vector3 setPosition = cardObject.transform.position + new Vector3(0, 0, -1);
+        //SelCard.transform.positionの位置にsetPositionを持っていく。
+        SelCard.transform.position = setPosition;
+
+         }
+      }
+
+
+  }
 }
