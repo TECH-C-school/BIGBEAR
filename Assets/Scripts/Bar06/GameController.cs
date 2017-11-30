067b1231fbd3420813df08cc5414f5c3d1a8d6b7
やたろう <karupisu.lobe.hanayo@gmail.com>
2017-11-30 17:27:46 +0900

    列挙型作成途中

--- Assets/Scripts/Bar06/GameController.cs
+++ Assets/Scripts/Bar06/GameController.cs
@@ -5,6 +5,53 @@ using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Bar06 {
     public class GameController : MonoBehaviour {
+
+        public enum Mark
+        {
+            Spade,
+            Heart,
+            Diamonds,
+            Club,
+        }
+
+        public enum Card
+        {
+            Ace,
+            two,
+            three,
+            four,
+            five,
+            six,
+            seven,
+            eight,
+            nine,
+            ten,
+            eleven,
+            twe
+        }
+
+        /// <summary>
+        /// プレイヤーとディーラーにカードを2枚づつ生成
+        /// </summary>
+        private void MakeCard()
+        {
+            int count = 0;
+            int[] randomNumbers = MakeRandomNumbers();
+            var cardPrefab = Resources.Load<GameObject>("Prefabs/Bar06/Card_back");
+            
+            for (var i = 0; i < 2; i++)
+            {
+                var cardObject = Instantiate(cardPrefab, transform.position, Quaternion.identity);
+                cardObject.transform.position = new Vector3(i * 1.27f - 2.54f, 0);
+     
+            }
+        }
+        private int[] MakeRandomNumbers()
+        {
+            int[] numbers = new int[52];
+            fo
+        }
+          
         public void TransitionToResult() {
             SceneManager.LoadScene("Result");
         }
