2f9f141b8b3117bc472dfb4465383b5b44ed8044
W301-06\student <16d3170001ae@edu.tech.ac.jp>
2017-11-30 20:21:11 +0900

    Add Bear Animation and Cartridge

--- Assets/Scripts/Game02/PlayerControll.cs
+++ Assets/Scripts/Game02/PlayerControll.cs
@@ -24,6 +24,8 @@ namespace Assets.Scripts.Game02
         [SerializeField]
         Button m_shotButton;
 
+
+
         // Use this for initialization
         void Start()
         {
@@ -45,37 +47,56 @@ namespace Assets.Scripts.Game02
 #endif
 #if UNITY_EDITOR
 
+            //Start
+            m_trigger.OnMouseDownAsObservable()
+                .Subscribe(pointerEventData =>
+                {
+
+                    m_scope.gameObject.SetActive(true);
+                    m_shotButton.interactable = true;
+
+                }).AddTo(this);
+
             m_trigger.OnMouseDragAsObservable()
                  .Subscribe(pointerEventData =>
                  {
+                     if (Input.touchCount > 1) return;
                      float x = Mathf.Sin(Time.time) /10 + (Mathf.Sin(Time.time) * Mathf.Sin(Time.time) / 10 * 2);
                      float y = Mathf.Cos(Time.time) /10 + (Mathf.Cos(Time.time) * Mathf.Cos(Time.time) / 10 * 2);
 
-                     m_swayObject.transform.position = new Vector3(x, y, 10);
+                     m_swayObject.transform.position = new Vector3(x, y, 10);        
 
-                     m_scope.gameObject.SetActive(true);
                      m_scope.transform.localPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x , (Input.mousePosition.y + (m_scope.sprite.rect.height/ 3f)), 10));
-                    
-                     RaycastHit2D hit = Physics2D.Raycast(m_scope.transform.position, m_scope.transform.forward, 10,m_enemyLayer);
-                     if (hit.collider)
-                     {
-                         
-                     }
-                   
 
+                     if (Input.GetKeyDown(KeyCode.Space))
+                     {
+                         ShotBullet();
+                     }
+                     
                  }).AddTo(this);
 
+            //End
             m_trigger.OnMouseUpAsObservable()
                 .Subscribe(pointerEventData =>
                 {
                     m_scope.gameObject.SetActive(false);
+                    m_shotButton.interactable = false;
+
                 }).AddTo(this);
 #endif
         }
 
         public void ShotBullet()
         {
-
+            RaycastHit2D hit = Physics2D.Raycast(m_scope.transform.position, m_scope.transform.forward, 10, m_enemyLayer);
+            if (hit.collider)
+            {
+                Debug.Log("hit");
+            }
+            else
+            {
+                Debug.Log("miss");
+            }
         }
 
 
