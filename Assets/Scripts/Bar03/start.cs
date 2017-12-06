3e5ebde209756d46071bcaee347131a97e21f978
岩崎友哉 <tomoti2525@yahoo.co.jp>
2017-12-07 00:34:18 +0900

    script変更

--- Assets/Scripts/Bar03/start.cs
+++ Assets/Scripts/Bar03/start.cs
@@ -70,14 +70,18 @@ public class start : MonoBehaviour {
         string[] numbers = {"c01", "c02", "c03", "c04", "c05", "c06", "c07", "c08", "c09", "c10", "c11", "c12", "c13",
                             "d01", "d02", "d03", "d04", "d05", "d06", "d07", "d08", "d09", "d10", "d11", "d12", "d13",
                             "h01", "h02", "h03", "h04", "h05", "h06", "h07", "h08", "h09", "h10", "h11", "h12", "h13",
-                            "s01", "s02", "s03", "s04", "s05", "s06", "s07", "s08", "s09", "s10", "s11", "s12", "s13"};
+                            "s01", "s02", "s03", "s04", "s05", "s06", "s07", "s08", "s09", "s10", "s11", "s12", "s13",
+                            "c01", "c02", "c03", "c04", "c05", "c06", "c07", "c08", "c09", "c10", "c11", "c12", "c13",
+                            "d01", "d02", "d03", "d04", "d05", "d06", "d07", "d08", "d09", "d10", "d11", "d12", "d13",
+                            "h01", "h02", "h03", "h04", "h05", "h06", "h07", "h08", "h09", "h10", "h11", "h12", "h13",
+                            "s01", "s02", "s03", "s04", "s05", "s06", "s07", "s08", "s09", "s10", "s11", "s12", "s13" };
         System.Random random = new System.Random();
-        int rnd = UnityEngine.Random.Range(1, 53);
         for (var i = 0; i < numbers.Length; i++)
         {
+            int rnd = UnityEngine.Random.Range(1, 105);
             var temp = numbers[i];
             numbers[i] = numbers[rnd];
-            numbers[rnd] = temp;
+            numbers[rnd] = temp;        
         }
         return numbers;
 
@@ -86,87 +90,80 @@ public class start : MonoBehaviour {
     {
         List<string> Row = new List<string>();
 
-        Row.Add(numbers[0]);
-        Row.Add("");
-        Row.Add("");
-        Row.Add("");
-        Row.Add("");
-        Row.Add("");
+        for(int i = 0; i < 6; i++)
+        {
+            Row.Add(numbers[i]);
+        }
 
         List<string> Row2 = new List<string>();
 
-        Row2.Add("");
-        Row2.Add("");
-        Row2.Add("");
-        Row2.Add("");
-        Row2.Add("");
-        Row2.Add("");
+        for (int i = 0; i < 6; i++)
+        {
+            Row2.Add(numbers[6 + i]);
+        }
 
         List<string> Row3 = new List<string>();
 
-        Row3.Add("");
-        Row3.Add("");
-        Row3.Add("");
-        Row3.Add("");
-        Row3.Add("");
-        Row3.Add("");
+        for (int i = 0; i < 6; i++)
+        {
+            Row3.Add(numbers[12 + i]);
+        }
 
         List<string> Row4 = new List<string>();
 
-        Row4.Add("");
-        Row4.Add("");
-        Row4.Add("");
-        Row4.Add("");
-        Row4.Add("");
-        Row4.Add("");
+        for (int i = 0; i < 6; i++)
+        {
+            Row4.Add(numbers[18 + i]);
+        }
 
         List<string> Row5 = new List<string>();
 
-        Row5.Add("");
-        Row5.Add("");
-        Row5.Add("");
-        Row5.Add("");
-        Row5.Add("");
+        for (int i = 0; i < 5; i++)
+        {
+            Row5.Add(numbers[24 + i]);
+        }
 
         List<string> Row6 = new List<string>();
 
-        Row6.Add("");
-        Row6.Add("");
-        Row6.Add("");
-        Row6.Add("");
-        Row6.Add("");
+        for (int i = 0; i < 5; i++)
+        {
+            Row6.Add(numbers[29 + i]);
+        }
 
         List<string> Row7 = new List<string>();
 
-        Row7.Add("");
-        Row7.Add("");
-        Row7.Add("");
-        Row7.Add("");
-        Row7.Add("");
+        for (int i = 0; i < 5; i++)
+        {
+            Row7.Add(numbers[34 + i]);
+        }
 
         List<string> Row8 = new List<string>();
 
-        Row8.Add("");
-        Row8.Add("");
-        Row8.Add("");
-        Row8.Add("");
-        Row8.Add("");
+        for (int i = 0; i < 5; i++)
+        {
+            Row8.Add(numbers[39 + i]);
+        }
 
 
         List<string> Row9 = new List<string>();
 
-        Row9.Add("");
-        Row9.Add("");
-        Row9.Add("");
-        Row9.Add("");
-        Row9.Add("");
+        for (int i = 0; i < 5; i++)
+        {
+            Row9.Add(numbers[44 + i]);
+        }
 
         List<string> Row10 = new List<string>();
 
-        Row10.Add("");
-        Row10.Add("");
-        Row10.Add("");
-        Row10.Add("");
-        Row10.Add("");
+        for (int i = 0; i < 5; i++)
+        {
+            Row10.Add(numbers[49 + i]);
+        }
+
+        List<string> Beck = new List<string>();
+
+        for (int i = 0; i < 50; i++)
+        {
+            Beck.Add(numbers[54 + i]);
+        }
     }
 }
