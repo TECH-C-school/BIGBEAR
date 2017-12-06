21aa08c786949a3f694052868c22c2f30b7ed833
Hara Kazunori <rize.game.0816@gmail.com>
2017-12-06 18:42:13 +0900

    スクリプト

--- Assets/Scripts/Bar06/GameController.cs
+++ Assets/Scripts/Bar06/GameController.cs
@@ -5,28 +5,44 @@ using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Bar06 {
     public class GameController : MonoBehaviour {
+
+
+        public enum Mark
+        {
+            Clover,
+            Spade,
+            Heart,
+            Diamond
+        }
+
+        public struct Card
+        {
+            public int number;
+            public Mark mark;
+        }
+
+        public void Start()
+        {
+            //カードを2, 2枚表示する
+            Card card1_1 = new Card();
+            Card card1_2 = new Card();
+            Card card2_1 = new Card();
+            Card card2_2 = new Card();
+
+            //ランダムでカードを決める
+            int number = Random.Range(1, 14);
+            int markValue = Random.Range(0, 4);
+            Mark mark = (Mark)markValue;
+            string str = mark.ToString();
+
+            Debug.Log(str + number);
+
+
+
+        }
+
         public void TransitionToResult() {
             SceneManager.LoadScene("Result");
-        }
-
-        private void card()
-        {
-            int number = 0;
-            string number2 = "";
-            string[] mark = new string[] { "h", "d", "s", "c"};
-            for (int i = 0; i <= 52; i++)
-            {
-                if (i >= 51)
-                {
-                    number2 = "";
-                } else
-                {
-                    number = i % 13;
-                    number2 = number.ToString();
-                }
-
-                Debug.Log(mark[i / 13] + number2);
-            }
-        }
+        }    
     }
 }
