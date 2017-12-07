400631c5b5d2a606845ef52adae376145ebc6e4e
mizumizo615 <mizu.615@i.softbank.jp>
2017-12-07 18:48:13 +0900

    ボタン作成（途中）

---
+++ Assets/Scripts/Bar04/Card Arrangement.cs
@@ -0,0 +1,40 @@
+﻿
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardArrangement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public class random : MonoBehaviour {
+        
+    //プレハブを変数に代入
+    public GameObject cube;
+
+
+    void Start () {
+
+
+        //オブジェクトの座標
+        table = Random.Range(-5.0f, 1.5f);
+        table(1) = Random.Range(-2.5f, 1.5f);
+        table(2) = Random.Range(0.0f, 1.5f);
+        table(3) = Random.Range(2.0f, 1.5f);
+        table(4) = Random.Range(5.0f, 1.5f);
+
+
+        //オブジェクトを生産
+        Instantiate(c01, c02, c03, c04, c05, c06, c07, c08, c09, c10, c11, c12, c13,
+            d01, d02, d03, d04, d05, d06, d07, d08, d09, d10, d11, d12, d13, 
+            h01, h02, h03, h04, h05, h06, h07, h08, h09, h10, h11, h12, h13, 
+            s01, s02, s03, s04, s05, s06, s07, s08, s09, s10, s11, s12, s13);
+        
+    }
+}
+}
+*/
--- Assets/Scripts/Bar04/GameController.cs
+++ Assets/Scripts/Bar04/GameController.cs
@@ -5,8 +5,19 @@ using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Bar04 {
     public class GameController : MonoBehaviour {
+
+        public GameObject Startbutton;
+       
         public void TransitionToResult() {
+
+
+  
             SceneManager.LoadScene("Result");
+        }
+
+        public void GameStart(){
+            Debug.Log("ゲームスタートです");
+            Startbutton.SetActive(false);
         }
     }
 }
