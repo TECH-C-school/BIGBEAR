af192d8f809da41598f03d21dd4797afe3b0ac95
Tomioka <tyuuni31@gmail.com>
2017-12-07 12:03:21 +0900

    いろいろした

--- Assets/Scripts/Bar06/Cards.cs
+++ Assets/Scripts/Bar06/Cards.cs
@@ -7,37 +7,15 @@ public class Cards : MonoBehaviour
 
     // Use this for initialization
     void Start() { }
-
-
-
-
-    /*private int[] MakeRandomNumbers() {
-        int[] numbers = new int[52];
-
-        for (int i = 0; i < numbers.Length; i++)
-        {
-            numbers[i] = i + 1;
-        }
-        var counter = 0;
-        while (counter < 52)
-        {
-            var index = Random.Range(counter, numbers.Length);
-            var tmp = numbers[counter];
-            numbers[counter] = numbers[index];
-            numbers[index] = tmp;
-
-
-            counter++;
-        }
-    }*/
+    
 }
-            public enum mark
-            {
-                S = 0,
-                C = 1,
-                H = 2,
-                D = 3
-            }
-            
+    public enum mark
+    {
+        S = 0,
+        C = 1,
+        H = 2,
+        D = 3
+    }
+
 
     
--- Assets/Scripts/Bar06/GameController.cs
+++ Assets/Scripts/Bar06/GameController.cs
@@ -4,6 +4,75 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Bar06 {
+
+
+    public class Cards : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        private void MakeCards()
+        {
+            int count = 0;
+            int[] randomNumbers = MakeRandomNumbers();
+
+            var cardPrefab = Resources.Load<GameObject>("Prefabs/Bar06/PlayerCardbk");
+            var cardObject = GameObject.Find("Cards");
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    var cardsObject = Instantiate(cardObject, transform.position, Quaternion.identity);
+                    cardObject.transform.position = new Vector3(
+                        i * 2f - 0.9f,
+                        j * 5f - 0.9f,
+                        1);
+                    var card = cardObject.GetComponent<Cards>();
+                    count++;
+                }
+            }
+        }
+
+
+
+
+
+        private int[] MakeRandomNumbers()
+        {
+            int[] numbers = new int[52];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i + 1;
+            }
+            var counter = 0;
+            while (counter < 52)
+            {
+                var index = Random.Range(counter, numbers.Length);
+                var tmp = numbers[counter];
+                numbers[counter] = numbers[index];
+                numbers[index] = tmp;
+
+
+                counter++;
+            }
+            return numbers;
+        }
+    }
+    public enum mark
+    {
+        S = 0,
+        C = 1,
+        H = 2,
+        D = 3
+    }
+
+
+
     public class GameController : MonoBehaviour {
         public void TransitionToResult() {
             SceneManager.LoadScene("Result");
