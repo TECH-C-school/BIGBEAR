6e3b33cf30a1e36c981b55a5c40581b587d7399d
koyama souichi <storm19980913@gmail.com>
2017-12-07 11:55:46 +0900

    背面表示

--- Assets/Scripts/Bar06/GameController.cs
+++ Assets/Scripts/Bar06/GameController.cs
@@ -14,7 +14,7 @@ namespace Assets.Scripts.Bar06 {
             Spade,
         }
 
-        public struct MakeCards
+        public struct Cards
         {
             public int number;
             public Mark mark;
@@ -23,44 +23,63 @@ namespace Assets.Scripts.Bar06 {
         //カード生成
         public void Start()
         {
-            MakeCards card1_1 = new MakeCards();
-            MakeCards card1_2 = new MakeCards();
-            MakeCards card2_1 = new MakeCards();
-            MakeCards card2_2 = new MakeCards();
-        }
+            Cards card1_1 = new Cards();
+            Cards card1_2 = new Cards();
+            Cards card2_1 = new Cards();
+            Cards card2_2 = new Cards();
 
-        //乱数作成
-        private int[] MakeRandomNumbers()
-        {
-            int[] numbers = new int[51];
-            for (int i = 0; i <= 51; i++)
-            {
-                numbers[i] = i + 1;
-            }
             var counter = 0;
-            while (counter < 51)
+            while (counter <= 3)
             {
-                var index = Random.Range(counter, numbers.Length);
-                var tmp = numbers[counter];
-                numbers[counter] = numbers[index];
-                numbers[index] = tmp;
-
-                counter++;
-
-                var a = index % 13 +1;
+                int number = Random.Range(1, 14);
+                int markV = Random.Range(0, 4);
+                Mark mark = (Mark)markV;
+                string str = mark.ToString();
+                Debug.Log(str + number);
             }
-            return numbers;
         }
-        //ボタン
-        public void ClickDrowButton()
+        public void MakeCards()
         {
-            var cardsObject = GameObject.Find("Cards").transform;
-            foreach (Transform cardObject in cardsObject)
+            var cardPrefab = Resources.Load<GameObject>("Prefabs/Bar06/card");
+            for(int y = 0; y < 2; y++)
             {
-                var card = cardsObject.GetComponent<MakeCards>();
-                
+                var cardObject = Instantiate(cardPrefab, transform.position, Quaternion.identity);
+                cardObject.transform.position = new Vector2(-1f + y, -2.5f);
             }
         }
+        //乱数作成
+        /* private int[] MakeRandomNumbers()
+         {
+             int[] numbers = new int[51];
+             for (int i = 0; i <= 51; i++)
+             {
+                 numbers[i] = i + 1;
+             }
+             var counter = 0;
+             while (counter < 51)
+             {
+                 var index = Random.Range(counter, numbers.Length);
+                 var tmp = numbers[counter];
+                 numbers[counter] = numbers[index];
+                 numbers[index] = tmp;
+
+                 counter++;
+
+                 var a = index % 13 +1;
+                 Debug.Log(Mark[a]+(a+1));
+             }
+             return numbers;
+         }
+         //ボタン
+         public void ClickDrowButton()
+         {
+             var cardsObject = GameObject.Find("Cards").transform;
+             foreach (Transform cardObject in cardsObject)
+             {
+                 var card = cardsObject.GetComponent<MakeCards>();
+
+             }
+         }*/
         public void TransitionToResult() {
             SceneManager.LoadScene("Result");
         }
