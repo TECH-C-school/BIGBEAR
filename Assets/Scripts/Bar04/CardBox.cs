305925bf7b2e3bf9d957db147537180b29f984b4
K-Tamura98 <17c1020017tk@edu.tech.ac.jp>
2017-12-07 12:05:15 +0900

    [Bar04_12]Card配列生成用スクリプトの作成(書きかけ)

---
+++ Assets/Scripts/Bar04/CardBox.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Assets.Scripts.Bar04
+{
+    public class CardBox : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
