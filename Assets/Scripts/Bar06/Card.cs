f9c5e0b732ec0adfc1cb0b8cb959eeb0de4c6a7e
久野　祥 <izayoi589@gmail.com>
2017-12-07 11:59:12 +0900

    画像表示

---
+++ Assets/Scripts/Bar06/Card.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+
+    public void RondomNum()
+    {
+        System.Random r = new System.Random();
+        int[] random = { };
+        int i, j, value;
+        for (i = 0; i < 45; i++){
+            random[i] = i + 1;
+        }
+        for (i = 0; i < 45; i++){
+            i = r.Next(45);
+            j = r.Next(45);
+            value = random[i];
+            random[i] = random[j];
+            random[j] = value;
+        }
+    }
+
+}
--- Assets/Scripts/Bar06/GameController.cs
+++ Assets/Scripts/Bar06/GameController.cs
@@ -5,6 +5,26 @@ using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Bar06 {
     public class GameController : MonoBehaviour {
+        private int count = 1;
+        public void Start()
+        {
+            var card = Resources.Load<GameObject>("Prefabs/Bar06/card");
+            var cardObject = Instantiate(card, transform.position, Quaternion.identity);
+            cardObject.transform.position = new Vector3(-1.5f,-1,0);
+
+        }
+        public void AddBottunClick()
+        {
+            
+            var card = Resources.Load<GameObject>("Prefabs/Bar06/card");
+            var cardObject = Instantiate(card, transform.position, Quaternion.identity);
+            cardObject.transform.position = new Vector3(-1.5f+1.5f*count, -1, 0);
+            count++;
+        }
+
+
+
+
         public void TransitionToResult() {
             SceneManager.LoadScene("Result");
         }
