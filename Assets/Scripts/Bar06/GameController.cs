0a0fe6c166e0832c3780542654acd5de11c38362
koyama souichi <storm19980913@gmail.com>
2017-12-13 18:31:55 +0900

    表示　クローン破壊作成中

--- Assets/Scripts/Bar06/GameController.cs
+++ Assets/Scripts/Bar06/GameController.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Bar06 {
     public class GameController : MonoBehaviour {
-
+        private int count = 1;
         public enum Mark
         {
             Clover,
@@ -36,52 +36,42 @@ namespace Assets.Scripts.Bar06 {
                 Mark mark = (Mark)markV;
                 string str = mark.ToString();
                 Debug.Log(str + number);
+                counter++;
             }
-        }
-        public void MakeCards()
-        {
+            //自分??
             var cardPrefab = Resources.Load<GameObject>("Prefabs/Bar06/card");
-            for(int y = 0; y < 2; y++)
+            for (int y = 0; y <= 1; y++)
             {
                 var cardObject = Instantiate(cardPrefab, transform.position, Quaternion.identity);
-                cardObject.transform.position = new Vector2(-1f + y, -2.5f);
+                cardObject.transform.position = new Vector3(0 - y, 1.5f, 0);
             }
+            //相手??
+            for (int y = 0; y <= 1; y++)
+            {
+                var cardObject = Instantiate(cardPrefab, transform.position, Quaternion.identity);
+                cardObject.transform.position = new Vector3(0 - y, -1.5f, 0);
+            }
+
         }
-        //乱数作成
-        /* private int[] MakeRandomNumbers()
-         {
-             int[] numbers = new int[51];
-             for (int i = 0; i <= 51; i++)
-             {
-                 numbers[i] = i + 1;
-             }
-             var counter = 0;
-             while (counter < 51)
-             {
-                 var index = Random.Range(counter, numbers.Length);
-                 var tmp = numbers[counter];
-                 numbers[counter] = numbers[index];
-                 numbers[index] = tmp;
 
-                 counter++;
-
-                 var a = index % 13 +1;
-                 Debug.Log(Mark[a]+(a+1));
-             }
-             return numbers;
-         }
-         //ボタン
-         public void ClickDrowButton()
-         {
-             var cardsObject = GameObject.Find("Cards").transform;
-             foreach (Transform cardObject in cardsObject)
-             {
-                 var card = cardsObject.GetComponent<MakeCards>();
-
-             }
-         }*/
-        public void TransitionToResult() {
+        public void Addcard()
+        {
+            var cardPrefab = Resources.Load<GameObject>("Prefabs/Bar06/card");
+            var cardObject = Instantiate(cardPrefab, transform.position, Quaternion.identity);
+            cardObject.transform.position = new Vector3(count, -1.5f, 0);
+            count++;
+        }
+        public void TransitionToResult()
+        {
             SceneManager.LoadScene("Result");
+        }
+        private void RemoveCards()
+        {
+            var cardsObject = GameObject.Find("Cards").transform;
+            foreach (Transform cardObject in cardsObject)
+            {
+                Destroy(cardObject.gameObject);
+            }
         }
     }
 }
