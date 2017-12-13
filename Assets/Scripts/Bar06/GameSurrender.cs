cb0577aa9cb3a09b048bfd923606c4837832cc48
sigkalra888 <sigkalra8888@gmail.com>
2017-12-13 18:50:08 +0900

    プレファブの追加　スクリプトの追加

---
+++ Assets/Scripts/Bar06/GameSurrender.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSurrender : MonoBehaviour {
+
+    public void Surrender()
+    {
+        Sprite coverScripts = null;
+        var coverPrefab = Resources.Load<GameObject>("Prefabs/Bar06/cover");
+        var coverObject = Instantiate(coverPrefab, transform.position, Quaternion.identity);
+        Vector3 move = transform.localPosition;
+        coverScripts = Resources.Load<Sprite>("Images/Bar/resultbg");
+        coverObject.transform.position = new Vector3(0, 0, 0);
+    }
+}
---
+++ Assets/Scripts/Bar06/hit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hit : CardMaster
+{
+    public void Hit()
+    {
+       
+    }
+	public void Start () {
+		
+	}
+	
+}
