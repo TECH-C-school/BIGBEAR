218edb544355c4a41ffb5ec42d5bd7027b3f26e2
matsuda <derby8923@icloud.com>
2017-12-14 18:49:09 +0900

    ちょっと進めた

--- Assets/Scripts/Bar01/Bar0104.cs
+++ Assets/Scripts/Bar01/Bar0104.cs
@@ -1,116 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class Bar0104: MonoBehaviour
-{
-    public string MakeRandomNunbers { get; private set; }
-    private const string KeyFasetestPlayTime = "FastestPlayTime";
-    private enum GameState
-    {
-        Prepare = 1,
-        Start,
-        finish
-    }
+public class Bar0104 : MonoBehaviour {
 
-    private int _nextCardNumber = 1;
-    private float _playTime = 0;
-    private float _fastestPlayTime = 0;
-    private GameState _gameState = GameState.Prepare;
-
-    // Use this for initialization
-    public GameObject Cards;
-    private object _fastestplayTime;
-
-    public string KeyFastestPlayTime { get; private set; }
-
-    void Start()
-    {
-        _fastestPlayTime = PlayerPrefs.GetFloat(KeyFasetestPlayTime, 5999.999f);
-        InitGame();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (_gameState == GameState.Start)
-        {
-            _playTime += Time.deltaTime;
-            ClickCard();
-            UpdatePlayTime();
-        }
-    }
-
-    /// <summary>
-    /// カードの生成
-    /// </summary>
-    private void MakeCards()
-    {
-
-        int count = 0;
-        int[] randomNumbers = RandomNunbers();
-
-        var cardPrefab = Resources.Load<GameObject>("Images/Bar/Cards");
-        var cardsObject = GameObject.Find("c01/c02/c03/c04/c05/c06/c07");
-
-        for (var i = 0; i < 6; i++)
-        {
-            for (var j = 0; j < 5; j++)
-            {                          
-                    var cardObject = Instantiate(cardPrefab, transform.position, Quaternion.identity);
-                    cardObject.transform.position = new Vector3(
-                        i * 1.8f - 5.3f,
-                        j * 1.8f - 4.3f,                      
-                        0);
-                    cardObject.transform.parent = Cards.transform;
-
-                    var card = cardObject.GetComponent<Card>();
-                    card.Number = randomNumbers[count];
-                    card.TurnCardFaceDown();
-                    count++;              
-            }
-        }
-    }
-
-    private int[] RandomNunbers()
-    {
-        int[] values = new int[52];
-        for (int z = 0; z < 52; z++)
-        {
-            values[z] = z + 1;
-        }
-
-        var counter = 0;
-        while (counter < 52)
-        {
-            var index = Random.Range(counter, values.Length);
-            var tmp = values[counter];
-            values[counter] = values[index];
-            values[index] = tmp;
-
-            counter++;
-        }
-        return values;
-    }
-
-
-    public void ClickStartButton()
-    {
-        var cardsobject = GameObject.Find("Cards").transform;
-        foreach (Transform cardobject in cardsobject.transform)
-        {
-            var card = cardobject.GetComponent<Card>();
-            card.TurnCardFaceUp();
-        }
-        _gameState = GameState.Start;
-    }
-
-    public void ClickResetButton()
-    {
-        InitGame();
-    }
-
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
     private void ClickCard()
     {
         //マウスクリックの判定
@@ -128,54 +30,6 @@ public class Bar0104: MonoBehaviour
 
         //クリックされたカードスクリプトを取得
         var card = hitObject.collider.gameObject.GetComponent<Card>();
-        Debug.Log("hit object is " + card.Number);
-
-        //次にクリックされるべきカードが判定
-        if (_nextCardNumber != card.Number) return;
-
-        //カードが反転する
-        card.TurnCardFaceDown();
-        _nextCardNumber++;
-
-        if (_nextCardNumber <= 52) return;
-
-        _gameState = GameState.finish;
-
-        if (_fastestPlayTime <= _playTime) return;
-
-        _fastestPlayTime = _playTime;
-        UpdateFastestPlayTime();
-
-        PlayerPrefs.SetFloat(KeyFastestPlayTime, _fastestPlayTime);
-    }
-    private void UpdatePlayTime()
-    {
-        var timelabel = GameObject.Find("Canvas/Panel/PlayTimeLabel").transform;
-        timelabel.GetComponent<Text>().text = _playTime.ToString();
-    }
-
-    private void UpdateFastestPlayTime()
-    {
-        var timelabel = GameObject.Find("Canvas/Panel/FastestPlayTimeLabel").transform;
-        timelabel.GetComponent<Text>().text = _fastestPlayTime.ToString();
-    }
-    private void InitGame()
-    {
-        _nextCardNumber = 1;
-        _playTime = 0;
-        _gameState = GameState.Prepare;
-
-        RemoveCards();
-        MakeCards();
-        UpdatePlayTime();
-    }
-
-    private void RemoveCards()
-    {
-        var cardsobject = GameObject.Find("back").transform;
-        foreach (Transform cardobject in cardsobject.transform)
-        {
-            Destroy(cardobject.gameObject);
-        }
+        Debug.Log("hit object is " + card.Number);                            
     }
 }
--- Assets/Scripts/Bar01/Card.cs
+++ Assets/Scripts/Bar01/Card.cs
@@ -1,19 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+    public class Card : MonoBehaviour {
 
-public class Card : MonoBehaviour
-{
+    Renderer Cardcorol;
 
-    private int _number;
+    string[] Cards = {
+            "c01","c02","c03","c04","c05","c06","c07","c08","c09","c10","c11","c12","c13",
+            "h01","h02","h03","h04","h05","h06","h07","h08","h09","h10","h11","h12","h13",
+            "d01","d02","d03","d04","d05","d06","d07","d08","d09","d10","d11","d12","d13",
+            "s01","s02","s03","s04","s05","s06","s07","s08","s09","s10","s11","s12","s13",           
+        };
+
+    int[] CardsNumber = {
+            1,2,3,4,5,6,7,8,9,10,11,12,13,
+            1,2,3,4,5,6,7,8,9,10,11,12,13,
+            1,2,3,4,5,6,7,8,9,10,11,12,13,
+            1,2,3,4,5,6,7,8,9,10,11,12,13,            
+        };
+
+    string[] CardsCollar = {
+           "Blcak","Blcak","Blcak","Blcak","Blcak","Blcak","Blcak","Blcak","Blcak","Blcak","Blcak","Blcak","Blcak",
+           "Red","Red","Red","Red","Red","Red","Red","Red","Red","Red","Red","Red","Red",
+           "Blcak","Blcak","Blcak","Blcak","Blcak","Blcak","Blcak","Blcak","Blcak","Blcak","Blcak","Blcak","Blcak",
+           "Red","Red","Red","Red","Red","Red","Red","Red","Red","Red","Red","Red","Red",
+       };
+
+    int n_Number;
 
     public int Number
     {
-        get { return _number; }
-        set { _number = value; }
+        get { return n_Number; }
+        set { n_Number = value; }
     }
-
-
     public void TurnCardFaceUp()
     {
         TurnCard(true);
@@ -24,28 +43,19 @@ public class Card : MonoBehaviour
         TurnCard(false);
     }
 
-    private void TurnCard(bool faceup)
+    private void TurnCard(bool FaceUp)
     {
-        Sprite CardSprite = null;
-        Sprite numberSprite = null;
+        Sprite Cardsprite = null;
 
-        if (faceup)
+        if (FaceUp)
         {
-            CardSprite = Resources.Load<Sprite>("Images/card_frontside");
-            numberSprite = Resources.Load<Sprite>("Images/" + _number);
+            Cardsprite = Resources.Load<Sprite>("Images/Bar/Cards/" + Cards[n_Number]);
         }
         else
         {
-            CardSprite = Resources.Load<Sprite>("Images/card_backside");
+            Cardsprite = Resources.Load<Sprite>("Images/Bar/Cards/back");
         }
-
         var spriteRenderer = transform.GetComponent<SpriteRenderer>();
-        spriteRenderer.sprite = CardSprite;
-
-        var numberObject = transform.Find("Nunber");
-        var numberSpriteRenderer = numberObject.GetComponent<SpriteRenderer>();
-        numberSpriteRenderer.sprite = numberSprite;
-
-
-    }
+        spriteRenderer.sprite = Cardsprite;
+    }   
 }
