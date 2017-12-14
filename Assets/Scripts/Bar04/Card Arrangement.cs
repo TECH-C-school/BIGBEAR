94eb98dc826b65ad15d5d38adb9187218a06f529
mizumizuo <mizu.615@i.softbank.jp>
2017-12-14 18:45:11 +0900

    プレハブ

--- Assets/Scripts/Bar04/Card Arrangement.cs
+++ Assets/Scripts/Bar04/Card Arrangement.cs
@@ -1,5 +1,5 @@
 ﻿
-/*
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,18 +14,60 @@ public class CardArrangement : MonoBehaviour {
     public class random : MonoBehaviour {
         
     //プレハブを変数に代入
-    public GameObject cube;
+    public GameObject cardback;
 
 
-    void Start () {
+        void Start() {
 
-
+        }
         //オブジェクトの座標
-        table = Random.Range(-5.0f, 1.5f);
-        table(1) = Random.Range(-2.5f, 1.5f);
-        table(2) = Random.Range(0.0f, 1.5f);
-        table(3) = Random.Range(2.0f, 1.5f);
-        table(4) = Random.Range(5.0f, 1.5f);
+        private void MakeCard()
+        {
+            int count = 0;
+            int[] randomNumbers = MakeRandomNumbers();
+            var cardPrefab = Resources.Load<GameObject>("Prefabs/Card");
+            var cardsObject = GameObject.Find("Cards");
+
+            for (var i = 0; i < 5; i++)
+            {
+                for (var j = 0; j < 5; j++)
+                {
+                    var cardObject = Instantiate(cardPrefab, transform.position, Quaternion.identity);
+                    cardObject.transform.position = new Vector3(
+                        i * 1.27f - 3.87f,
+                        j * 1.27f - 2.54f,
+                        0);
+                    cardObject.transform.parent = cardsObject.transform;
+
+                    var card = cardObject.GetComponent<Card>();
+                    card.Number = randomNumbers[count];
+                    card.TurnCardFaceDown();
+                    count++;
+
+                }
+            }
+        }
+
+        private int[] MakeRandomNumbers()
+        {
+            int[] numbers = new int[52];
+            for (var i = 0; i < 52; i++)
+            {
+                numbers[i] = i + 1;
+            }
+
+            var counter = 0;
+            while (counter < 52)
+            {
+                var index = Random.Range(counter, numbers.Length);
+                var tmp = numbers[counter];
+                numbers[counter] = numbers[index];
+                numbers[index] = tmp;
+
+                counter++;
+            }
+            return numbers;
+        
 
 
         //オブジェクトを生産
@@ -36,5 +78,4 @@ public class CardArrangement : MonoBehaviour {
         
     }
 }
-}
-*/
+
