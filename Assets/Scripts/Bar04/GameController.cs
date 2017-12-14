acfc7156a46e11c4dfa99904ffef5ddd3f98f9e5
fujitaryusuke <ryusuke0428tora@gmail.com>
2017-12-14 18:46:24 +0900

    プレハブ生成、ソースコード

--- Assets/Scripts/Bar04/GameController.cs
+++ Assets/Scripts/Bar04/GameController.cs
@@ -15,5 +15,49 @@ namespace Assets.Scripts.Bar04 {
             Debug.Log("ゲームスタート");
             Startbutton.SetActive(false);
         }
+        //カード生成
+        private void MakeCard()
+        {
+            int count = 0;
+            int[] randomNumbers = MakeRandomNumbers();
+            var cardPrefab = Resources.Load<GameObject>("Prefabs/Card");
+            var cardsObject = GameObject.Find("Cards");
+
+            for (var i = 0; i < 5; i++)
+            {
+                for (var j = 0; j < 5; j++)
+                {
+                    var cardObject = Instantiate(cardPrefab, transform.position, Quaternion.identity);
+                    cardObject.transform.position = new Vector3(
+                        i * 1.27f - 3.87f,
+                        j * 1.27f - 2.54f,
+                        0);
+                    cardObject.transform.parent = cardsObject.transform;
+
+                }
+            }
+        }
+
+        //52枚をランダムにする
+        private int[] MakeRandomNumbers()
+        {
+            int[] numbers = new int[52];
+            for (var i = 0; i < 52; i++)
+            {
+                numbers[i] = i + 1;
+            }
+
+            var counter = 0;
+            while (counter < 52)
+            {
+                var index = Random.Range(counter, numbers.Length);
+                var tmp = numbers[counter];
+                numbers[counter] = numbers[index];
+                numbers[index] = tmp;
+
+                counter++;
+            }
+            return numbers;
+        }
     }
 }
