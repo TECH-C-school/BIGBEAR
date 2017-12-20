375e66c0e59e587426ff232236c9c2adc5567738
やたろう <karrupisu.lobe.hanayo@gmail.com>
2017-12-20 15:19:48 +0900

    作った物の削除。（1からリスタート）

--- Assets/Scripts/Bar06/GameController.cs
+++ Assets/Scripts/Bar06/GameController.cs
@@ -9,67 +9,9 @@ namespace Assets.Scripts.Bar06
     {
         void Start()
         {
-            MakeCard();
 
         }
         private void Update()
-        {
-
-        }
-
-
-        /// <summary>
-        /// カードを生成する
-        /// </summary>
-        private void MakeCard()
-        {
-            var cardPrefab = Resources.Load<GameObject>("Prefabs/Bar06/back");
-            for (var i = 0; i < 2; i++)
-            {
-                for (var j = 0; j < 2; j++)
-                {
-                    var cardObject = Instantiate(cardPrefab, transform.position, Quaternion.identity);
-                    cardObject.transform.position = new Vector2(i - 0.5f, j * 2.25f - 1.1f);
-                    cardObject.transform.parent = transform;
-                }
-            }
-        }
-
-        /// <summary>
-        /// クリックしたときカードを追加する
-        /// </summary>
-        public void ClickCardplusButton()
-        {
-            var cardPrefab = Resources.Load<GameObject>("Prefabs/Bar06/c01");
-                var cardObject = Instantiate(cardPrefab, transform.position, Quaternion.identity);
-                cardObject.transform.position = new Vector2(0f,0f);
-                cardObject.transform.parent = transform;
-        }
-        /// <summary>
-        /// クリックしたとき相手とのカードの大きさを比べる
-        /// </summary>
-        public void ClickBattleButton()
-        {
-
-        }
-        /// <summary>
-        /// クリックしたとき勝負をあきらめる
-        /// </summary>
-        public void ClickSalenderButton()
-        {
-
-        }
-        /// <summary>
-        /// クリックしたときベットを1づつ減らす
-        /// </summary>
-        public void ClickBetmainusButton()
-        {
-
-        }
-        /// <summary>
-        /// クリックしたときベットを1づつ増やす
-        /// </summary>
-        public void ClickBetplusButton()
         {
 
         }
