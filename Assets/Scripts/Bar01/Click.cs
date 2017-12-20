7a6ce1d47293e5d4aaef202db1accdad51ce377f
DESKTOP-CLISS3A\s_u_k <suzuki527@docomo.ne.jp>
2017-12-20 15:34:17 +0900

    カード移動のスクリプト編集

--- Assets/Scripts/Bar01/Click.cs
+++ Assets/Scripts/Bar01/Click.cs
@@ -2,51 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Click : MonoBehaviour {
+public class Click : MonoBehaviour
+{
 
-	
-	void Start ()
+    void Start()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         ClickCard();
-	}
+    }
 
     private void ClickCard()
     {
-        float Range = 100f;
+        var InputPosition = Input.mousePosition;
 
-        var MousePosition = Input.mousePosition;
+        if (!Input.GetMouseButton(0)) return;
 
-        if (!Input.GetMouseButtonDown(0)) return;
+        var TapPoint = Camera.main.ScreenToWorldPoint(InputPosition);
+        Debug.Log(TapPoint);
 
-        var TapPoint = Camera.main.ScreenToWorldPoint(MousePosition);
-        Debug.Log(gameObject.name);
+        if (!Physics2D.OverlapPoint(TapPoint)) return;
+        Debug.Log("hogehoge");
 
-        RaycastHit hit = new RaycastHit();
-        /*
-                if (Physics.Raycast(TapPoint, out hit, Range))
-                {
+        var HitObject = Physics2D.Raycast(TapPoint, -Vector3.up);
+        Debug.Log(HitObject.transform.name);
 
-                }
+        TapPoint.z = -14;
+        if (!HitObject) return;
 
-                RaycastHit Hit = new RaycastHit();
-                Debug.Log(Hit);
-                Debug.Log(Hit.collider);
-                Hit.collider.gameObject.transform.position = TapPoint;
-
-                var Hitcard = Physics2D.Raycast(TapPoint, -Vector2.up);
-                if (!Hitcard) return;
-
-
-
-
-                var CardName = Hitcard.collider.gameObject.GetComponent<Cards>();
-                Debug.Log("hit card is" + Cards.Number);
-                */
+        HitObject.transform.position = new Vector3(TapPoint.x, TapPoint.y, -14);
+        Debug.Log(HitObject.transform.position);
     }
 }
