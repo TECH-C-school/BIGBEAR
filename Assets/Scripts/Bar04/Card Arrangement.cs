9a67b22e183607b3df97ec9d037ba070f03d43e9
mizumizuo <mizu.615@i.softbank>
2017-12-20 13:21:45 +0900

    スクリプトの編集

--- Assets/Scripts/Bar04/Card Arrangement.cs
+++ Assets/Scripts/Bar04/Card Arrangement.cs
@@ -4,20 +4,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardArrangement : MonoBehaviour {
+public class CardArrangement : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-    public class random : MonoBehaviour {
-        
-    //プレハブを変数に代入
-    public GameObject cardback;
+    }
+
+    public class random : MonoBehaviour
+    {
+
+        //プレハブを変数に代入
+        public GameObject cardback;
 
 
-        void Start() {
+        void Start()
+        {
 
         }
         //オブジェクトの座標
@@ -39,11 +43,11 @@ public class CardArrangement : MonoBehaviour {
                         0);
                     cardObject.transform.parent = cardsObject.transform;
 
-                    var card = cardObject.GetComponent<Card>();
+                    /*var card = cardObject.GetComponent<Card>();
                     card.Number = randomNumbers[count];
                     card.TurnCardFaceDown();
                     count++;
-
+                    */
                 }
             }
         }
@@ -67,15 +71,16 @@ public class CardArrangement : MonoBehaviour {
                 counter++;
             }
             return numbers;
-        
 
 
-        //オブジェクトを生産
-        Instantiate(c01, c02, c03, c04, c05, c06, c07, c08, c09, c10, c11, c12, c13,
-            d01, d02, d03, d04, d05, d06, d07, d08, d09, d10, d11, d12, d13, 
-            h01, h02, h03, h04, h05, h06, h07, h08, h09, h10, h11, h12, h13, 
-            s01, s02, s03, s04, s05, s06, s07, s08, s09, s10, s11, s12, s13);
-        
+
+            //オブジェクトを生産
+            /*Instantiate(c01, c02, c03, c04, c05, c06, c07, c08, c09, c10, c11, c12, c13,
+                d01, d02, d03, d04, d05, d06, d07, d08, d09, d10, d11, d12, d13, 
+                h01, h02, h03, h04, h05, h06, h07, h08, h09, h10, h11, h12, h13, 
+                s01, s02, s03, s04, s05, s06, s07, s08, s09, s10, s11, s12, s13);
+            */
+        }
     }
 }
 
