2f48f35194da071ecc2099f1e9af759275b10253
koyama souichi <storm19980913@gmail.com>
2018-01-11 11:40:50 +0900

    fight

--- Assets/Scripts/Bar06/GameController.cs
+++ Assets/Scripts/Bar06/GameController.cs
@@ -2,35 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System;
-using System.IO;
 
-namespace Assets.Scripts.Bar06 {
-    public class GameController : MonoBehaviour {
-        private int PC = 1;
-        private int playercount = 0;
-        private int enemycount = 0;
-        private int DeckCount = 0;
+namespace Assets.Scripts.Bar06
+{
+    public class GameController : MonoBehaviour
+    {
+        private int ep = 0;
+        private int PC = 2;
+        private int pc = 0;
+        private int ec = 0;
+        private int DC = 0;
         private int[] numbers = new int[52];
         private string[] mark = new string[52];
         private int i, j, k, value;
         private string val;
 
-        public void Start()
+        public void MakeDeck()
         {
             System.Random random = new System.Random();
-            for (i = 0,j = 0; i < 52; i++, j++)
+            for (i = 0, j = 0; i < 52; i++, j++)
             {
                 if (i < 13)
                 {
                     mark[i] = "c";
-                }else if (i < 26)
+                }
+                else if (i < 26)
                 {
                     mark[i] = "d";
-                }else if (i < 39)
+                }
+                else if (i < 39)
                 {
                     mark[i] = "h";
-                }else
+                }
+                else
                 {
                     mark[i] = "s";
                 }
@@ -41,7 +45,7 @@ namespace Assets.Scripts.Bar06 {
                 }
                 numbers[i] = j + 1;
             }
-            for(i = 0; i < 52; i++)
+            for (i = 0; i < 52; i++)
             {
                 j = random.Next(52);
                 k = random.Next(52);
@@ -52,51 +56,124 @@ namespace Assets.Scripts.Bar06 {
                 mark[j] = mark[k];
                 mark[k] = val;
             }
+        }
+        public void Start()
+        {
+            MakeDeck();
             //player
-            for(i = 0; i < 2; i++)
+            for (i = 0; i < 2; i += 1)
             {
-                var playerCardPrefab = Resources.Load<GameObject>("Prefabs/Bar06/" + mark[DeckCount] + numbers[DeckCount]);
-                var playerCard = Instantiate(playerCardPrefab, transform.position, Quaternion.identity);
-                playerCard.transform.position = new Vector2(0 - i, -1.5f);
-                playerCard.transform.localScale = new Vector2(0.17f, 0.17f);
-                DeckCount++;
+                var pCP_1 = Resources.Load<GameObject>("Prefabs/Bar06/" + mark[DC] + numbers[DC]);
+                var pC_1 = Instantiate(pCP_1, transform.position, Quaternion.identity);
+                pC_1.transform.position = new Vector2(0 - i, -1.5f);
+                pC_1.transform.localScale = new Vector2(0.17f, 0.17f);
+                if (numbers[DC] >= 11)
+                {
+                    numbers[DC] = 10;
+                }
+                if (numbers[DC] == 1)
+                {
+                    if (pc < 11)
+                    {
+                        numbers[DC] = 11;
+                    }
+                    else
+                    {
+                        numbers[DC] = 1;
+                    }
+                }
+                pc = pc + numbers[DC];
+                DC++;
             }
             //enemy
-            var enemyCardPrefab_1 = Resources.Load<GameObject>("Prefabs/Bar06/");
-        }
+            var eCP_1 = Resources.Load<GameObject>("Prefabs/Bar06/card");
+            var eC_1 = Instantiate(eCP_1, transform.position, Quaternion.identity);
+            eC_1.transform.position = new Vector2(0, 1.5f);
+            eC_1.transform.localScale = new Vector2(0.17f, 0.17f);
 
-        public void Addcard()
-        {
-           if (playercount <= 21)
+            var eCP_2 = Resources.Load<GameObject>("Prefabs/Bar06/" + mark[DC] + numbers[DC]);
+            var eC_2 = Instantiate(eCP_2, transform.position, Quaternion.identity);
+            eC_2.transform.position = new Vector2(-1, 1.5f);
+            eC_2.transform.localScale = new Vector2(0.17f, 0.17f);
+            if (numbers[DC] >= 11)
             {
-                var test = 1;
-                var cardPrefab = Resources.Load<GameObject>("Prefabs/Bar06/c0"+test);
-                var AddcardObject = Instantiate(cardPrefab, transform.position, Quaternion.identity);
-                AddcardObject.transform.position = new Vector2(PC, -2.5f);
-                AddcardObject.transform.localScale = new Vector2(0.17f, 0.17f);
-                PC++;
-                var playerCard = UnityEngine.Random.Range(1, 14);
-                playercount = playercount + playerCard;
-                Debug.Log(playercount);
+                numbers[DC] = 10;
             }
-        }
-
-        public void Battle()
-        {
-            Debug.Log(enemycount);
-        }
-
-        public void TransitionToResult()
-        {
-            SceneManager.LoadScene("Result");
-        }
-        private void RemoveCards()
-        {
-            var cardsObject = GameObject.Find("Cards").transform;
-            foreach (Transform cardObject in cardsObject)
+            if (numbers[DC] == 1)
             {
-                Destroy(cardObject.gameObject);
+                if (ec < 11)
+                {
+                    numbers[DC] = 11;
+                }
+                else
+                {
+                    numbers[DC] = 1;
+                }
+            }
+            ec = ec + numbers[DC];
+            DC++;
+        }
+        public void AddCard()
+        {
+            if (pc <= 21)
+            {
+                var aCP = Resources.Load<GameObject>("Prefabs/Bar06/" + mark[DC] + numbers[DC]);
+                var aC = Instantiate(aCP, transform.position, Quaternion.identity);
+                aC.transform.position = new Vector2(-1 + PC, -1.5f);
+                aC.transform.localScale = new Vector2(0.17f, 0.17f);
+                PC += 1;
+                if (numbers[DC] >= 11)
+                {
+                    numbers[DC] = 10;
+                }
+                if (numbers[DC] == 1)
+                {
+                    if (pc < 11)
+                    {
+                        numbers[DC] = 11;
+                    }
+                    else
+                    {
+                        numbers[DC] = 1;
+                    }
+                }
+                pc = pc + numbers[DC];
+                DC ++;
+                if (pc >= 22)
+                {
+                    fight();
+                }
+            }
+
+        }
+        public void fight()
+        {
+            while (ec <= 16)
+            {
+                var aECP = Resources.Load<GameObject>("Prefabs/Bar06/" + mark[DC] + numbers[DC]);
+                var aEC = Instantiate(aECP, transform.position, Quaternion.identity);
+                aEC.transform.position = new Vector2(0 + ep, 1.5f);
+                aEC.transform.localScale = new Vector2(0.17f, 0.17f);
+                ep ++;
+                if (numbers[DC] >= 11)
+                {
+                    numbers[DC] = 10;
+                }
+                if (numbers[DC] == 1)
+                {
+                    if (ec < 11)
+                    {
+                        numbers[DC] = 11;
+                    }
+                    else
+                    {
+                        numbers[DC] = 1;
+                    }
+                }
+                ec = ec + numbers[DC];
+                DC++;
             }
         }
     }
 }
+ 
