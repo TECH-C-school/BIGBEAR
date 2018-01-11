c711fd4838adcec0e9b8a43649e4c3247e0c859a
瀧川　僚 <frkz1998@gmail.com>
2018-01-11 11:52:13 +0900

    カード生成

--- Assets/Scripts/Bar06/GameController.cs
+++ Assets/Scripts/Bar06/GameController.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace Assets.Scripts.Bar06 {
-    public class GameController : MonoBehaviour {
-        public void TransitionToResult() {
-            Sprite[] image = Resources.LoadAll<Sprite>("Images/Bar/Cards/");
+namespace Assets.Scripts.Bar06
+{
+    public class GameController : MonoBehaviour
+    {
+        public GameObject canvas;//キャンバス
+        public GameObject cards_bg;
 
-            SceneManager.LoadScene("Result");
+        void Start()
+        {
+            // プレハブを取得
+            GameObject prefab = (GameObject)Resources.Load("Prefabs/Bar06/cards_bg");
+            // プレハブからインスタンスを生成
+            Instantiate(prefab);
+
+
         }
     }
 }
