9329eb7e627c65a977a0e2e921155cb75ebc9a59
302-16 <gundam@gmail.com>
2018-01-11 15:31:28 +0900

    敵の攻撃途中、無敵機能の一部バグ

---
+++ Assets/Scripts/Game08/Enemy/Enemysikai.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemysikai : MonoBehaviour {
+
+    public LayerMask mask;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Ray()
+    {
+        // Rayの作成
+        Ray ray = new Ray(transform.position, new Vector3(0, -10, 0));
+
+        // Rayが衝突したコライダーの情報を得る
+        RaycastHit hit;
+
+        
+    }
+
+
+
+}
---
+++ Assets/Scripts/Game08/Player/Mutekitime.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mutekitime : MonoBehaviour {
+
+	private GameObject _enemi;	// 敵の弾を入れる変数
+	public bool _on_damage = false;	// ダメージフラグ
+	private SpriteRenderer _renderer;
+
+	// Use this for initialization
+	void Start () {
+
+		// 点滅処理の為に呼び出しておく
+		_renderer = gameObject.GetComponent<SpriteRenderer>();
+		_enemi = GameObject.Find ("08_shell2"); // 変数にオブジェクト代入
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// ダメージフラグがtrueで有れば点滅させる
+		if (_on_damage) 
+		{
+			float _level = Mathf.Abs (Mathf.Sin (Time.time * 10));
+			_renderer.color = new Color (1f, 1f, 1f, _level);
+		}
+	}
+
+    // 敵とぶつかった際の処理
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        //  敵とぶつかったかつダメージフラグがfalse
+        if(!_on_damage && _enemi)
+        {
+            OnDamage();
+        }
+    }
+
+
+    // ダメージを受けた際
+    void OnDamage()
+	{
+		// ダメージフラグon
+		_on_damage = true;
+
+        // コルーチン開始
+        StartCoroutine("WaitForIt");
+    }
+
+    IEnumerator WaitForIt()
+    {
+        // 1秒間処理を止める
+        yield return new WaitForSeconds(3);
+
+        // 1秒後ダメージフラグをfalseにして点滅を戻す
+        _on_damage = false;
+        _renderer.color = new Color(1f, 1f, 1f, 1f);
+    }
+	
+
+}
