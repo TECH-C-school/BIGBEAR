9dd403d5b7e79d2d69d643e2224c7fc9aac0ce69
コウシコウ <qq592662941@gmail.com>
2018-01-11 18:44:53 +0900

    簡単なヤツを完成

--- Assets/Scripts/Bar07/GameController.cs
+++ Assets/Scripts/Bar07/GameController.cs
@@ -25,14 +25,20 @@ namespace Assets.Scripts.Bar07 {
         GameObject coin3_text;
         GameObject money;
         GameObject Buttonshoufu;
+        GameObject coin1;
+        GameObject coin2;
+        GameObject coin3;
+        GameObject playerwin;
+        GameObject bankerwin;
+        GameObject history;
         public Text text1;
         public Text text2;
         public Text text3;
         public Text text4;
         public Text text5;
         public int mode = 1;
-        public int a;
-        public int b;
+        public int a=0;
+        public int b=0;
         public string c = "";
         public int d;
         public int e;
@@ -47,8 +53,7 @@ namespace Assets.Scripts.Bar07 {
         public int c4 = 0;
         public int c5 = 0;
         public int c6 = 0;
-
-
+        public string h;
 
 
 
@@ -71,9 +76,16 @@ namespace Assets.Scripts.Bar07 {
             coin1_text = GameObject.Find("coin1_text");
             coin2_text = GameObject.Find("coin2_text");
             coin3_text = GameObject.Find("coin3_text");
+            coin1 = GameObject.Find("coin1");
+            coin2 = GameObject.Find("coin2");
+            coin3 = GameObject.Find("coin3");
             money = GameObject.Find("money");
-            a = 10000;
-            b = 1;
+            playerwin = GameObject.Find("playerwin");
+            bankerwin = GameObject.Find("bankerwin");
+            history = GameObject.Find("history");
+
+            int a = 10000;
+            int b = 1;
             
 
 
@@ -114,7 +126,7 @@ namespace Assets.Scripts.Bar07 {
 
                 kakekin.SetActive(true);
                 shengfu.SetActive(false);
-                Buttonsaihaichi.SetActive(true);
+                Buttonsaihaichi.SetActive(false);
                 Buttonreset.SetActive(true);
                 Buttonshoufu.SetActive(true);
                 winsu.SetActive(false);
@@ -126,16 +138,24 @@ namespace Assets.Scripts.Bar07 {
                 card6.SetActive(false);
                 back1.SetActive(true);
                 back2.SetActive(true);
+                coin1.SetActive(true);
+                coin2.SetActive(true);
+                coin3.SetActive(true);
 
             }
             if (mode == 0)
             {
                 kakekin.SetActive(false);
                 shengfu.SetActive(true);
-                Buttonsaihaichi.SetActive(false);
+                Buttonsaihaichi.SetActive(true);
                 Buttonreset.SetActive(false);
                 Buttonshoufu.SetActive(false);
                 winsu.SetActive(true);
+                coin1.SetActive(false);
+                coin2.SetActive(false);
+                coin3.SetActive(false);
+                back1.SetActive(false);
+                back2.SetActive(false);
 
 
 
@@ -233,25 +253,32 @@ namespace Assets.Scripts.Bar07 {
 
 
             string card11 = card_1.Substring(1, card_1.Length-1);
-            int.TryParse(card_1,out c1);
-            int.TryParse(card_2, out c2);
-            int.TryParse(card_3, out c3);
-            int.TryParse(card_4, out c4);
-            int.TryParse(card_5, out c5);
-            int.TryParse(card_6, out c6);
-            Debug.Log(card_1);
-            Debug.Log(card_2);
-            Debug.Log(card_3);
-            Debug.Log(card_4);
-            Debug.Log(card_5);
-            Debug.Log(card_6);
-            Debug.Log(c1);
-            Debug.Log(c2);
-            Debug.Log(c3);
-            Debug.Log(c4);
-            Debug.Log(c5);
-            Debug.Log(c6);
+            string card22 = card_2.Substring(1, card_2.Length - 1);
+            string card33 = card_3.Substring(1, card_3.Length - 1);
+            string card44 = card_4.Substring(1, card_4.Length - 1);
+            string card55 = card_5.Substring(1, card_5.Length - 1);
+            string card66 = card_6.Substring(1, card_6.Length - 1);
 
+            int.TryParse(card11, out c1);
+            int.TryParse(card22, out c2);
+            int.TryParse(card33, out c3);
+            int.TryParse(card44, out c4);
+            int.TryParse(card55, out c5);
+            int.TryParse(card66, out c6);
+            if(c1>=10)
+            { c1 = 0; }
+            if (c2 >= 10)
+            { c2 = 0; }
+            if (c3 >= 10)
+            { c3 = 0; }
+            if (c4 >= 10)
+            { c4 = 0; }
+            if (c5 >= 10)
+            { c5 = 0; }
+            if (c6 >= 10)
+            { c6 = 0; }
+         
+            
 
 
         }
@@ -294,16 +321,352 @@ namespace Assets.Scripts.Bar07 {
             coin3_text.GetComponent<Text>().text = z.ToString();
             money.GetComponent<Text>().text = a.ToString();
         }
-
+        public void re()
+        {
+            x = 0;
+            y = 0;
+            z = 0;
+            coin1_text.GetComponent<Text>().text = x.ToString();
+            coin2_text.GetComponent<Text>().text = y.ToString();
+            coin3_text.GetComponent<Text>().text = z.ToString();
+        }
         public void saihaichi()
         {
+            mode = 1;
+            modechange();
             haifu();
-            riset();
+            re();
         }
 
         public void shoufu()
         {
             
+           int player = c2 + c3;
+            if (player > 10)
+            {
+                player = player - 10;
+            }
+           int banker = c5 + c6;
+            if (banker > 10)
+            {
+                banker = banker - 10;
+            }
+            if (player > 8|| banker>8 )
+            {
+                card1.SetActive(false);
+                card2.SetActive(true);
+                card3.SetActive(true);
+                card4.SetActive(false);
+                card5.SetActive(true);
+                card6.SetActive(true);
+                string his;
+                if (player > banker)
+                {
+                    a = a + 2 * x;
+                    money.GetComponent<Text>().text = a.ToString();
+                    d = d + 1;
+                    playerwin.GetComponent<Text>().text = d.ToString();
+                    his = "P";
+                   
+                    h = his+h;
+                    history.GetComponent<Text>().text = h;
+
+                }
+
+
+                else if (player < banker)
+                {
+                    a = a + 2 * z;
+                    money.GetComponent<Text>().text = a.ToString();
+                    e = e + 1;
+                    bankerwin.GetComponent<Text>().text = e.ToString();
+                    his = "B";
+                    h = his + h;
+                    history.GetComponent<Text>().text = h;
+                }
+                else
+                {
+                    a = a + 9 * y;
+                    money.GetComponent<Text>().text = a.ToString();
+                    his = "D";
+                    h = his + h;
+                    history.GetComponent<Text>().text = h;
+                }
+
+
+            }
+            else if((player==5||player==6)&& (banker == 5 ||banker == 6))
+            {
+                card1.SetActive(false);
+                card2.SetActive(true);
+                card3.SetActive(true);
+                card4.SetActive(false);
+                card5.SetActive(true);
+                card6.SetActive(true);
+                string his;
+                if (player > banker)
+                {
+                    a = a + 2 * x;
+                    money.GetComponent<Text>().text = a.ToString();
+                    d = d + 1;
+                    playerwin.GetComponent<Text>().text = d.ToString();
+                    his = "P";
+                    h = his + h;
+                    history.GetComponent<Text>().text = h;
+
+                }
+
+
+                else if (player < banker)
+                {
+                    a = a + 2 * z;
+                    money.GetComponent<Text>().text = a.ToString();
+                    e = e + 1;
+                    bankerwin.GetComponent<Text>().text = e.ToString();
+                    his = "B";
+                    h = his + h;
+                    history.GetComponent<Text>().text = h;
+                }
+                else
+                {
+                    a = a + 9 * y;
+                    money.GetComponent<Text>().text = a.ToString();
+                    his = "D";
+                    h = his + h;
+                    history.GetComponent<Text>().text = h;
+                }
+
+
+            }
+            else
+            {
+                if (player < 6)
+                {
+                    player = player + c1;
+                    if (player > 10)
+                    {
+                        player = player - 10;
+                    }
+                    card1.SetActive(true);
+                    card2.SetActive(true);
+                    card3.SetActive(true);
+                    card5.SetActive(true);
+                    card6.SetActive(true);
+                    switch (c1)
+                    {
+                        case 0:if(banker<4)
+                            {
+                                banker = banker + c4;
+                                if (banker > 10)
+                                {
+                                    banker = banker - 10;
+                                }
+                                card4.SetActive(true);
+                            }
+                            else
+                            {
+                                card4.SetActive(false);
+                            }
+                            break;
+                        case 1:
+                            if (banker < 4)
+                            {
+                                banker = banker + c4;
+                                if (banker > 10)
+                                {
+                                    banker = banker - 10;
+                                }
+                                card4.SetActive(true);
+                            }
+                            else
+                            {
+                                card4.SetActive(false);
+                            }
+                            break;
+                        case 2:
+                            if (banker < 5)
+                            {
+                                banker = banker + c4;
+                                if (banker > 10)
+                                {
+                                    banker = banker - 10;
+                                }
+                                card4.SetActive(true);
+                            }
+                            else
+                            {
+                                card4.SetActive(false);
+                            }
+                            break;
+                        case 3:
+                            if (banker < 5)
+                            {
+                                banker = banker + c4;
+                                if (banker > 10)
+                                {
+                                    banker = banker - 10;
+                                }
+                                card4.SetActive(true);
+                            }
+                            else
+                            {
+                                card4.SetActive(false);
+                            }
+                            break;
+                        case 4:
+                            if (banker < 6)
+                            {
+                                banker = banker + c4;
+                                if (banker > 10)
+                                {
+                                    banker = banker - 10;
+                                }
+                                card4.SetActive(true);
+                            }
+                            else
+                            {
+                                card4.SetActive(false);
+                            }
+                            break;
+                        case 5:
+                            if (banker < 6)
+                            {
+                                banker = banker + c4;
+                                if (banker > 10)
+                                {
+                                    banker = banker - 10;
+                                }
+                                card4.SetActive(true);
+                            }
+                            else
+                            {
+                                card4.SetActive(false);
+                            }
+                            break;
+                        case 6:
+                            if (banker < 7)
+                            {
+                                banker = banker + c4;
+                                if (banker > 10)
+                                {
+                                    banker = banker - 10;
+                                }
+                                card4.SetActive(true);
+                            }
+                            else
+                            {
+                                card4.SetActive(false);
+                            }
+                            break;
+                        case 7:
+                            if (banker < 7)
+                            {
+                                banker = banker + c4;
+                                if (banker > 10)
+                                {
+                                    banker = banker - 10;
+                                }
+                                card4.SetActive(true);
+                            }
+                            else
+                            {
+                                card4.SetActive(false);
+                            }
+                            break;
+                        case 8:
+                            if (banker < 3)
+                            {
+                                banker = banker + c4;
+                                if (banker > 10)
+                                {
+                                    banker = banker - 10;
+                                }
+                                card4.SetActive(true);
+                            }
+                            else
+                            {
+                                card4.SetActive(false);
+                            }
+                            break;
+
+                        case 9:
+                            if (banker < 4)
+                            {
+                                banker = banker + c4;
+                                if (banker > 10)
+                                {
+                                    banker = banker - 10;
+                                }
+                                card4.SetActive(true);
+                            }
+                            else
+                            {
+                                card4.SetActive(false);
+                            }
+                            break;
+                    }
+
+
+                }else
+                {
+                    card1.SetActive(false);
+                    card2.SetActive(true);
+                    card3.SetActive(true);
+                    card5.SetActive(true);
+                    card6.SetActive(true);
+                    if (banker < 6)
+                    {
+                        banker = banker + c4;
+                        if (banker > 10)
+                        {
+                            banker = banker - 10;
+                        }
+                        card4.SetActive(true);
+                    }
+                    else
+                    {
+                        card4.SetActive(false);
+                    }
+                }
+                string his;
+                if (player > banker)
+                {
+                    a = a + 2 * x;
+                    money.GetComponent<Text>().text = a.ToString();
+                    d = d + 1;
+                    playerwin.GetComponent<Text>().text = d.ToString();
+                    his = "P";
+                    h = his+h;
+                    history.GetComponent<Text>().text = h;
+
+                }
+
+
+                else if (player < banker)
+                {
+                    a = a + 2 * z;
+                    money.GetComponent<Text>().text = a.ToString();
+                    e = e + 1;
+                    bankerwin.GetComponent<Text>().text = e.ToString();
+                    his = "B";
+                    h = his+h;
+                    history.GetComponent<Text>().text = h;
+                }
+                else
+                {
+                    a = a + 9 * y;
+                    money.GetComponent<Text>().text = a.ToString();
+                    his = "D";
+                    h = his + h;
+                    history.GetComponent<Text>().text = h;
+                }
+
+
+            }
+
+
+
+
 
 
 
@@ -311,6 +674,10 @@ namespace Assets.Scripts.Bar07 {
             modechange();
         }
 
+        
+
+
+        
 
 
 
