a2cdc1d549333be86d53d555649ef5fbe4aa96cf
masashitakada <masapo.0315@icloud.com>
2018-01-12 17:18:21 +0900

    カードをドラッグで移動させるコードを少しだけ書き換えた

--- Assets/Scripts/Bar03/MouseDrag.cs
+++ Assets/Scripts/Bar03/MouseDrag.cs
@@ -1,12 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseDrag : MonoBehaviour {
-    
+
+    public Vector3 startposition;
+    private void Start()
+    {
+        //開始時の座標を取得
+        startposition = gameObject.transform.position;
+    }
+
     //マウスドラッグでオブジェクトを動かす
     void OnMouseDrag()
     {
+        startposition = transform.position;
         Vector3 objectPointInScreen
             = Camera.main.WorldToScreenPoint(this.transform.position);
 
@@ -19,8 +28,11 @@ public class MouseDrag : MonoBehaviour {
         mousePointInWorld.z = this.transform.position.z;
         this.transform.position = mousePointInWorld;
 
-    }
+        //ドラッグを離したら元の位置に戻る
+        if ()
+        {
+            gameObject.transform.position = startposition;
+        }
 
-    
-    
+    }
 }
