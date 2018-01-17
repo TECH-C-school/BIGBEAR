459070729a8939a688aa3de7a79a9cebfac782b2
KimitaSora <minecraft.nichijo@ezweb.ne.jp>
2018-01-17 18:57:06 +0900

    Script作成

--- Assets/Scripts/Bar04/Card.cs
+++ Assets/Scripts/Bar04/Card.cs
@@ -12,7 +12,7 @@ namespace Assets.Scripts.Bar04
 
         private LayerMask mask;
         private int alpha;
-        private Queue<int> hogehoge;
+        //private Queue<int> hogehoge;
         // Use this for initialization
 
 
@@ -46,38 +46,44 @@ namespace Assets.Scripts.Bar04
 
         }
 
-        //作成したデッキの上からカードを5枚出す
+        ////作成したデッキの上からカードを5枚出す
 
             
 
       
 
-        //ランダムに出した5つの数値を、カードの画像に当てはめる
+        ////ランダムに出した5つの数値を、カードの画像に当てはめる
        
-        public enum Suit
-        {
-            SPADE,
-            HEART,
-            CLUB,
-            DIA
-        }
+        //public enum Suit
+        //{
+        //    SPADE,
+        //    HEART,
+        //    CLUB,
+        //    DIA
+        //}
 
-        public Suit suit;
+        //public Suit suit;
 
-        void Start()
-        {
+        //void Start()
+        //{
             
-        }
+        //}
 
         //void Update()
         //{
-        //    if (hogehoge < 12) ;
+            
+        //}
+
+        //private  hand card()
+        //{
 
         //}
-        //SPADE(s01~s13)
-        //HEART(h01~h13)
-        //CLUB(c01~c13)
-        //DIA(d01~d13)
+
+
+        ////SPADE(s01~s13)(0~12)
+        ////HEART(h01~h13)(13~25)
+        ////CLUB(c01~c13)(26~38)
+        ////DIA(d01~d13)(39~51)
 
 
 
--- Assets/Scripts/Bar04/GameController.cs
+++ Assets/Scripts/Bar04/GameController.cs
@@ -24,8 +24,8 @@ namespace Assets.Scripts.Bar04
         [SerializeField]
         private GameObject cardSelect;
 
-        private Queue<int> hogehoge;
-        private Queue<int> decks;
+        [SerializeField]
+        private int[] cards;
 
 
 
@@ -37,24 +37,29 @@ namespace Assets.Scripts.Bar04
         }
 
 
-        
-
-
-
+        //数値に名前を付ける(カードの名前)
+        [SerializeField]
+        private string[] str =
+            {"s01","s02","s03","s04","s05","s06","s07","s08","s09","s10","s11","s12","s13",
+             "h01","h02","h03","h04","h05","h06","h07","h08","h09","h10","h11","h12","h13",
+             "c01","c02","c03","c04","c05","c06","c07","c08","c09","c10","c11","c12","c13",
+             "d01","d02","d03","d04","d05","d06","d07","d08","d09","d10","d11","d12","d13"};
+          
+         
 
         //カード複製 or 配置
         private void MakeCards()
         {
   
-            //52枚のカードをランダムに生成
-            int[] cards = new int[51];
-            for (var i = 0; i < 51; i++)
+            //52枚の数値をランダムに生成
+            cards = new int[52];
+            for (int i = 0; i < cards.Length; i++)
             {
-                cards[i] = i + 1;
+                cards[i] = i;
             }
 
             var counter = 0;
-            while (counter < 51)
+            while (counter < cards.Length)
             {
                 var index = Random.Range(counter, cards.Length);
                 var tmp = cards[counter];
@@ -64,38 +69,37 @@ namespace Assets.Scripts.Bar04
                 counter++;
             }
 
-            //ランダム作成したものを順番に入れる
-            Queue<int> decks = new Queue<int>() { };
-            for (var i = 0; i < 51; i++)
-            {
-                decks.Enqueue(cards[i]);
-            }
-            
+
 
             //カードを５枚複製する
             for (int i = 0; i < 5; i++)
             {
                 //デッキの上から5枚取り出す
-                while (decks.Count < 41)
-                {
-                 /*   decud*/
-                }
+                 //while (cards.Length < 41)
+                 //{
+                 // /*   decud*/
+                 //}
 
 
                 //カードを並べる
                 var cardObject = Instantiate(cardPrefab, transform.position, Quaternion.identity);
+
                 cardObject.transform.position = new Vector2(i * 1.8f - 3.84f, 0);
 
-                //手札を反転する
+                var spriteRenderer = cardObject.GetComponent<SpriteRenderer>();
+                spriteRenderer.sprite = Resources.Load<Sprite>("Images/Bar/Cards/" + str[cards[i]]);
                 
+
+                //カードを反転する
+
+                //Card.TuranCardFaceDown();
+
+
+
                 //SpriteRenderer
 
                 GameObject SelCard = Instantiate(cardSelect);
-                SelCard.SetActive(false);
 
-
-                //Selcard(選択したときに光る)生成
-                 SelCard = Instantiate(cardSelect);
 
                 //配置時のSelcardの透明度の設定
                 var image = SelCard.GetComponent<SpriteRenderer>();
@@ -114,6 +118,9 @@ namespace Assets.Scripts.Bar04
 
             }
         }
+
+        //役作成
+
 
 
 
