a6220066096f36676cbbcdd52610cad625304d86
Jason <jstai7887bleach@live.com>
2018-01-22 02:23:56 +0900

    .

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -5,66 +5,103 @@ using System;
 
 public class NewBehaviourScript : MonoBehaviour {
 
-    // Use this for initialization
-    void Start() {
-        Debug.Log(cardName);
-        makeShuffledDeck();
+    public GameObject cardPrefab;
+    public Transform playerTrans, enemyTrans, bankerTrans;
 
-        for (int i = 1; i < 52; i++)
+    CurrCard[] defaultCardsArray, shuffeledArray;
+    CurrCard[] playerArray, enemyArray, bankerArray;
+
+    int usedIndex = 0;
+    bool isGiven = false;
+
+    class CurrCard
+    {
+        public Cards cardValue;
+        public Transform cardTrans;
+
+        public CurrCard(Cards cardValue, Transform cardTrans)
         {
-            string v = Enum.GetName(typeof(Cards), i);
-            Debug.Log(v);
+            this.cardValue = cardValue;
+            this.cardTrans = cardTrans;
         }
-
-        MakeCards();
     }
-
-    // Update is called once per frame
-    void Update() {
-
-    }
-    public enum Cards
+    enum Cards
     {
         c01 = 1, c02, c03, c04, c05, c06, c07, c08, c09, c10, c11, c12, c13,
         d01, d02, d03, d04, d05, d06, d07, d08, d09, d10, d11, d12, d13,
-        a01, a02, a03, a04, a05, a06, a07, a08, a09, a10, a11, a12, a13,
+        h01, h02, h03, h04, h05, h06, h07, h08, h09, h10,ha11, h12, h13,
         s01, s02, s03, s04, s05, s06, s07, s08, s09, s10, s11, s12, s13
     }
-    private string cardName = Enum.GetName(typeof(Cards), 1);
-
-    public int[] makeShuffledDeck()
+    void Start()
     {
-        int[] numbers = new int[52];
-        for (int i = 0; i < 52; i++)
-        {
-            numbers[i] = i + 1;
-        }
+        int deckLength = Enum.GetNames(typeof(Cards)).Length;
+        defaultCardsArray = new CurrCard[deckLength];
 
-        var counter = 0;
-        while (counter < 52)
+        CreateDefaultDeck();
+        shuffeledArray = makeShuffledDeck(defaultCardsArray);
+    }  
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isGiven && Input.GetMouseButtonDown(0))
         {
-            var index = UnityEngine.Random.Range(counter, numbers.Length);
-            var tmp = numbers[counter];
-            numbers[counter] = numbers[index];
-            numbers[index] = tmp;
-            counter++;
+            GiveCards(playerTrans, 2);
+            GiveCards(enemyTrans, 2);
+            GiveCards(bankerTrans, 5);
+            isGiven = true;
         }
-
-        for (int i = 0; i < 52; i++)
+    }
+   
+    void CreateDefaultDeck()
+    {
+        //Putting cards into shuffledArray.
+        int i = 0;
+        foreach(Cards card in Enum.GetValues(typeof(Cards)))
         {
-            Debug.Log(numbers[i]);
+            Sprite sprite = Resources.Load("Images/Bar/Cards/" + card, typeof(Sprite)) as Sprite;
+            GameObject newObj = Instantiate(cardPrefab, transform.position, Quaternion.identity);
+            newObj.GetComponent<SpriteRenderer>().sprite = sprite;
+            //Debug.Log(sprite);
+            CurrCard currCard = new CurrCard(card, newObj.transform);
+            defaultCardsArray[i] = currCard;
+            defaultCardsArray[i].cardTrans.gameObject.SetActive(false);
+            i++;
         }
-        return numbers;
     }
 
-    private void MakeCards()
+    CurrCard[] makeShuffledDeck(CurrCard[] shuffledArray)
     {
-        var cardPrefab = Resources.Load<GameObject>("Images/Bar/Cards/ + _cardName");
-        var cardsObject = GameObject.Find("Card");
+        int counter = 0;
+        while(counter < 52)
+        {
+            int index = UnityEngine.Random.Range(counter, shuffledArray.Length);
+            Cards tmp = shuffledArray[counter].cardValue;
+            shuffledArray[counter] = shuffledArray[index];
+            shuffledArray[index].cardValue = tmp;
+            counter++;
+        }
+        return shuffledArray;
+    }
 
-        var cardObject = Instantiate(cardPrefab, transform.position, Quaternion.identity);
-        cardObject.transform.position = new Vector3(
-            1, 1, 0);
+   void GiveCards(Transform target, int noOfCards)
+    {
+        CurrCard[] tempArray = new CurrCard[noOfCards];
 
+        for(int i = 0; i < noOfCards; i++)
+        {
+            int temp = usedIndex + 1;
+            tempArray[i] = shuffeledArray[temp];
+        }
+        usedIndex += noOfCards;
+
+        if (target.name == "PlayerPos") playerArray = tempArray;
+        else if (target.name == "EnemyPos") enemyArray = tempArray;
+        else bankerArray = tempArray;
+
+        for(int i = 0; i < noOfCards; i++)
+        {
+            tempArray[i].cardTrans.position = target.position;
+            tempArray[i].cardTrans.gameObject.SetActive(true);
+        }
     }
 }
