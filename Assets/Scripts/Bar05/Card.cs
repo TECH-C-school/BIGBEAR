b2b3ad8f3ee1efd0a91aa2ada2ef9dc501c33b80
koumon <760443380>
2018-01-24 21:51:42 +0900

    直す

---
+++ Assets/Scripts/Bar05/Card.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    //花色 0黑桃 1红心 2方块 3梅花
+    public int suit;
+    //数字 1 - 52
+    public int number;
+    //图片
+    public SpriteRenderer spr;
+}
