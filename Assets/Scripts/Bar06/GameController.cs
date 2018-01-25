d7a07744ad1c57bf1357cb842d4b57f7af2748fb
Tomioka <tyuuni31@gmail.com>
2018-01-25 12:01:49 +0900

    エラーの処理終わらない

--- Assets/Scripts/Bar06/GameController.cs
+++ Assets/Scripts/Bar06/GameController.cs
@@ -10,12 +10,13 @@ namespace Assets.Scripts.Bar06
 
         int[] cards = new int[52];
 
+
+
+        [SerializeField]
+
         string[] Deck =
             {
-            "s01","s02","s03","s04","s05","s06","s07","s08","s09","s10","s11","s12","s13",
-            "h01","h02","h03","h04","h05","h06","h07","h08","h09","h10","h11","h12","h13",
-            "c01","c02","c03","c04","c05","c06","c07","c08","c09","c10","c11","c12","c13",
-            "d01","d02","d03","d04","d05","d06","d07","d08","d09","d10","d11","d12","d13",
+            "s01","s02","s03","s04","s05","s06","s07","s08","s09","s10","s11","s12","s13","h01","h02","h03","h04","h05","h06","h07","h08","h09","h10","h11","h12","h13","c01","c02","c03","c04","c05","c06","c07","c08","c09","c10","c11","c12","c13","d01","d02","d03","d04","d05","d06","d07","d08","d09","d10","d11","d12","d13",
             };
 
         int count = 0;
@@ -23,9 +24,11 @@ namespace Assets.Scripts.Bar06
 
 
 
+
         // ランダムな山札作る
         private void MakeYamahuda()
         {
+
             for (int i = 0; i < cards.Length; i++)
             {
                 cards[i] = i + 1;
@@ -39,9 +42,16 @@ namespace Assets.Scripts.Bar06
                 int tmp = cards[k];
                 cards[k] = cards[n];
                 cards[n] = tmp;
-
             }
         }
+
+
+           
+
+
+
+
+
         // ２枚のカードを配る
         private void CardsKubaru()
         {
@@ -54,15 +64,23 @@ namespace Assets.Scripts.Bar06
         private void LoadingCards(int x, int y, int z)
         {
             var card = GameObject.Find("Cards");
-            
+
             var Card = Resources.Load<Sprite>("Prefabs/Bar06/Cards/" + Deck[cards[count]]);
             var Dummy = Instantiate(card, new Vector2(y - 2f, z * 4 - 1.2f), Quaternion.identity);
             //Card.transform.parent = card.transform;
             count++;
 
             //Dummy （ダミー）　←なにこれ
-            
+
         }
+
+        struct card
+        {
+            char mark;
+            int num;
+        };
+
+        
 
 
         void Start()
@@ -95,57 +113,32 @@ namespace Assets.Scripts.Bar06
     }
 }
 
+
+
+
 /*
- * // カードを２枚ずつ配る
-        private void Dealcards()
+ 
+     void Start()
         {
-            for(int i = 0; i < 4; i++)
-            {
-               //Debug.Log(cards[i]);
+            MakeDeck();
+            Dealcards();
+            int x = 0;
+            for (var i = 0; i < 2; i++)
+            { 
+                for (var j = 0; j < 2; j++)
+                {
+                    if(i == 1)
+                    {
+                        i++;
+                        x = 1;
+                    }
+                    LoadCards(cards[i + j], i - x, j);
+                }
             } 
         }
-        // カードを表示する
-        private void LoadCards(int x, int y,int z)
-        {
-            var Card = Resources.Load<GameObject>("Prefabs/Bar06/Cards/" + CardsName[x-1]);
-            Card = Instantiate(Card, new Vector2(y - 0.5f, z * 2 - 1f), Quaternion.identity);
-            var card = GameObject.Find("Cards");
-            Card.transform.parent = card.transform;
-                 if(x == 14 || x == 27 || x == 40){x = 1;}
-            else if(x == 15 || x == 28 || x == 41){x = 2;}
-            else if(x == 16 || x == 29 || x == 42){x = 3;}
-            else if(x == 17 || x == 30 || x == 43){x = 4;}
-            else if(x == 18 || x == 31 || x == 44){x = 5;}
-            else if(x == 19 || x == 32 || x == 45){x = 6;}
-            else if(x == 20 || x == 33 || x == 46){x = 7;}
-            else if(x == 21 || x == 34 || x == 47){x = 8;}
-            else if(x == 22 || x == 35 || x == 48){x = 9;}
-            else if(x == 23 || x == 36 || x == 49){x = 10;}
-            else if(x == 24 || x == 37 || x == 50){x = 10;}
-            else if(x == 25 || x == 38 || x == 51){x = 10;}
-            else if(x == 26 || x == 39 || x == 52){x = 10;}
-            Debug.Log(x);
-        }
- 
-     {
-            for (int i = 0; i < cards.Length; i++)
-            {
-                cards[i] = i + 1;
-            }
-            System.Random rng = new System.Random();
-            int n = cards.Length;
-            while (n > 1)
-            {
-                n--;
-                int k = rng.Next(n + 1);
-                int tmp = cards[k];
-                cards[k] = cards[n];
-                cards[n] = tmp;
-                
-            }
-        }
-
-    public void ClickAddCardButton()
+     
+             // クリックしたらカードを追加する
+       /* public void ClickAddCardButton()
         {
             
             var Card = Resources.Load<GameObject>("Prefabs/Bar06/Cards/" + CardsName[x - 1]);
@@ -153,6 +146,42 @@ namespace Assets.Scripts.Bar06
             var card = GameObject.Find("Cards");
             Card.transform.parent = card.transform;
             Debug.Log(x);
-        }
-     
+
+
+                //4枚のカードの表示
+            var counterForTrump = 0;
+            var cardsObject = GameObject.Find("PlayerCard").transform;
+            foreach (Transform cardObject in cardsObject.transform)
+            {
+                var fileName = "";
+
+                switch (trump[counterForTrump].mark)
+                {
+                    case Mark.S:
+                        fileName += "s";
+                        break;
+                    case Mark.C:
+                        fileName += "c";
+                        break;
+                    case Mark.H:
+                        fileName += "h";
+                        break;
+                    default:
+                        fileName += "d";
+                        break;
+                }
+
+                if (trump[counterForTrump].number < 10)
+                {
+                    fileName += "0" + trump[counterForTrump].number;
+                }
+                else
+                {
+                    fileName += trump[counterForTrump].number;
+                }
+
+
+
+
+
      */
